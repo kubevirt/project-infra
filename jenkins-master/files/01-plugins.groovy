909dbc921764531ea2ef2249b4c3fd9f36292ffc
Roman Mohr <rmohr@redhat.com>
2017-06-06 13:27:54 +0200

    Initial commit
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ jenkins-master/files/01-plugins.groovy
@@ -0,0 +1,37 @@
+import jenkins.model.*
+import java.util.logging.Logger
+
+def logger = Logger.getLogger("")
+def installed = false
+def initialized = false
+
+def pluginParameter="github-oauth swarm"
+def plugins = pluginParameter.split()
+logger.info("" + plugins)
+def instance = Jenkins.getInstance()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+uc.updateAllSites()
+
+plugins.each {
+  logger.info("Checking " + it)
+  if (!pm.getPlugin(it)) {
+    logger.info("Looking UpdateCenter for " + it)
+    if (!initialized) {
+      uc.updateAllSites()
+      initialized = true
+    }
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      logger.info("Installing " + it)
+    	plugin.deploy()
+      installed = true
+    }
+  }
+}
+
+if (installed) {
+  logger.info("Plugins installed, initializing a restart!")
+  instance.save()
+  instance.doSafeRestart()
+}
