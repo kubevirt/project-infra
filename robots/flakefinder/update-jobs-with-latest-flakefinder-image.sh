00a3fe15fd07be92279378cafc77f0ad3a73adaf
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-12-17 09:47:41 +0100

    Fix script that updates flakefinder image shas in periodics (#272)
    The script only updated the shas for kubevirt/kubevirt, this fix now
    updates any *-periodics.yaml below github/ci/prow/files/jobs .
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- robots/flakefinder/update-jobs-with-latest-flakefinder-image.sh
+++ robots/flakefinder/update-jobs-with-latest-flakefinder-image.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 docker pull kubevirtci/flakefinder
 sha_id=$(docker images --digests kubevirtci/flakefinder | grep 'latest ' | awk '{ print $3 }')
 
-sed -i -E 's/index\.docker\.io\/kubevirtci\/flakefinder@sha256\:[a-z0-9]+/'"index\.docker\.io\/kubevirtci\/flakefinder@$sha_id"'/g' \
-    ../../github/ci/prow/files/jobs/kubevirt/kubevirt-periodics.yaml
+for file in $(grep -l 'image: .*/kubevirtci/flakefinder' ../../github/ci/prow/files/jobs/**/*-periodics.yaml); do
+    sed -i -E 's/index\.docker\.io\/kubevirtci\/flakefinder@sha256\:[a-z0-9]+/'"index\.docker\.io\/kubevirtci\/flakefinder@$sha_id"'/g' \
+        "$file"
+done
