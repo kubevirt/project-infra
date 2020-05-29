ac68296904d74efd652e2274411e7ec2c84393cd
Roman Mohr <rmohr@redhat.com>
2020-05-29 09:53:17 +0200

    Fix flakefinder bump script
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- robots/flakefinder/update-jobs-with-latest-flakefinder-image.sh
+++ robots/flakefinder/update-jobs-with-latest-flakefinder-image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 docker pull kubevirtci/flakefinder
 sha_id=$(docker images --digests kubevirtci/flakefinder | grep 'latest ' | head -1 | awk '{ print $3 }')
 
-for file in $(grep -l 'image: .*/kubevirtci/flakefinder' ../../github/ci/prow/files/jobs/**/*-periodics.yaml); do
+for file in $(grep -l 'image: .*/kubevirtci/flakefinder' ../../github/ci/prow/files/jobs/**/**/*-periodics.yaml); do
     sed -i -E 's/index\.docker\.io\/kubevirtci\/flakefinder@sha256\:[a-z0-9]+/'"index\.docker\.io\/kubevirtci\/flakefinder@$sha_id"'/g' \
         "$file"
 done
