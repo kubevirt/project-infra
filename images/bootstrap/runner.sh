56ab681833b13e4f35b3a716dc8ddc4163c3b0b5
Roman Mohr <rmohr@redhat.com>
2021-01-28 10:10:50 +0100

    A new fedora based bootstrap image (#894)
    * Initial import of bootstrap from test-infra
    
    Taken from https://github.com/kubernetes/test-infra/commit/7e74b33534f950170801fb3ce015c1e96c975471.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Remove unrelated files
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Rework test-infra boostrap scripts for fedora
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Add custom certificate setup to runner.sh
    
    The certificates are used for container registry proxies.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Cache the most commonly used bazel versions in the new image
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Replace the old bootstrap image with the new one
    
    In addition set our greenhouse instance as the default cache.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Update copyright notice
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * The runner script sets the docker storage location
    
    Don't create a docker configuration file which will not be used.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Create bazel cache key via rpm commands
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Update comments in the bazel cache creation script
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- images/bootstrap/runner.sh
+++ images/bootstrap/runner.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-# Copyright 2020 The KubeVirt Authors.
+#!/usr/bin/env bash
+# Copyright 2018 The Kubernetes Authors.
+# Copyright 2021 The KubeVirt Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,21 +14,119 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+# generic runner script, handles DIND, bazelrc for caching, etc.
 
+# Check if the job has opted-in to bazel remote caching and if so generate 
+# .bazelrc entries pointing to the remote cache
+export BAZEL_REMOTE_CACHE_ENABLED=${BAZEL_REMOTE_CACHE_ENABLED:-false}
+if [[ "${BAZEL_REMOTE_CACHE_ENABLED}" == "true" ]]; then
+    echo "Bazel remote cache is enabled, generating .bazelrcs ..."
+    /usr/local/bin/create_bazel_cache_rcs.sh
+fi
+
+# setup custom certificates for the container registry mirror
 setup_ca(){
     if [ -f "${CA_CERT_FILE}" ]; then
         echo "Adding ${CA_CERT_FILE} as a trusted root CA"
-        cp "${CA_CERT_FILE}" /usr/local/share/ca-certificates/
+        cp "${CA_CERT_FILE}" /etc/pki/ca-trust/source/anchors/
 
-        update-ca-certificates
+        update-ca-trust
     fi
 }
 
-main(){
-    setup_ca
-
-    /usr/local/bin/runner_orig.sh "${@}"
+# runs custom docker data root cleanup binary and debugs remaining resources
+cleanup_dind() {
+    if [[ "${DOCKER_IN_DOCKER_ENABLED:-false}" == "true" ]]; then
+        echo "Cleaning up after docker"
+        docker ps -aq | xargs -r docker rm -f || true
+        kill "$(</var/run/docker.pid)" || true
+        wait "$(</var/run/docker.pid)" || true
+    fi
 }
 
-main "${@}"
+early_exit_handler() {
+    cleanup_dind
+}
+
+# setup certificates before anything gets started
+setup_ca
+
+# optionally enable ipv6 docker
+export DOCKER_IN_DOCKER_IPV6_ENABLED=${DOCKER_IN_DOCKER_IPV6_ENABLED:-false}
+if [[ "${DOCKER_IN_DOCKER_IPV6_ENABLED}" == "true" ]]; then
+    echo "Enabling IPV6 for Docker."
+    # configure the daemon with ipv6
+    mkdir -p /etc/docker/
+    cat <<EOF >/etc/docker/daemon.json
+{
+  "ipv6": true,
+  "fixed-cidr-v6": "fc00:db8:1::/64"
+}
+EOF
+    # enable ipv6
+    sysctl net.ipv6.conf.all.disable_ipv6=0
+    sysctl net.ipv6.conf.all.forwarding=1
+    # enable ipv6 iptables
+    modprobe -v ip6table_nat
+fi
+
+# Check if the job has opted-in to docker-in-docker availability.
+export DOCKER_IN_DOCKER_ENABLED=${DOCKER_IN_DOCKER_ENABLED:-false}
+if [[ "${DOCKER_IN_DOCKER_ENABLED}" == "true" ]]; then
+    echo "Docker in Docker enabled, initializing..."
+    printf '=%.0s' {1..80}; echo
+    # If we have opted in to docker in docker, start the docker daemon,
+    /usr/bin/dockerd -p /var/run/docker.pid --data-root=/docker-graph >/var/log/dockerd.log 2>&1 &
+    # the service can be started but the docker socket not ready, wait for ready
+    WAIT_N=0
+    MAX_WAIT=5
+    while true; do
+        # docker ps -q should only work if the daemon is ready
+        docker ps -q > /dev/null 2>&1 && break
+        if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
+            WAIT_N=$((WAIT_N+1))
+            echo "Waiting for docker to be ready, sleeping for ${WAIT_N} seconds."
+            sleep ${WAIT_N}
+        else
+            echo "Reached maximum attempts, not waiting any longer..."
+            break
+        fi
+    done
+    printf '=%.0s' {1..80}; echo
+    echo "Done setting up docker in docker."
+fi
+
+trap early_exit_handler INT TERM
+
+# disable error exit so we can run post-command cleanup
+set +o errexit
+
+# add $GOPATH/bin to $PATH
+export PATH="${GOPATH}/bin:${PATH}"
+mkdir -p "${GOPATH}/bin"
+# Authenticate gcloud, allow failures
+if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]]; then
+  gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}" || true
+fi
+
+# Use a reproducible build date based on the most recent git commit timestamp.
+SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct || true)
+export SOURCE_DATE_EPOCH
+
+# actually start bootstrap and the job
+set -o xtrace
+"$@"
+EXIT_VALUE=$?
+set +o xtrace
+
+# cleanup after job
+if [[ "${DOCKER_IN_DOCKER_ENABLED}" == "true" ]]; then
+    echo "Cleaning up after docker in docker."
+    printf '=%.0s' {1..80}; echo
+    cleanup_dind
+    printf '=%.0s' {1..80}; echo
+    echo "Done cleaning up after docker in docker."
+fi
+
+# preserve exit value from job / bootstrap
+exit ${EXIT_VALUE}
