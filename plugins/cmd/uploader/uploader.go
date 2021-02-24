91ff4d6810cd3039e28281b89b8ec522ab5e37cb
Roman Mohr <rmohr@redhat.com>
2021-02-24 14:57:24 +0100

    Make the dependency mirror tool more fault  tolerant (#970)
    * Don't upload error body on failed http gets
    
    If we don't get a 2XX response, we should not updload the returned body.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Verify shasum of rpm source
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Add a flag to continue uploads on error
    
    The exit code of the command will still be non-zero in case that an
    artifact could not be uploaded.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Add a command to verify all mirrored artifacts
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>
    
    * Use authenticated client for verification if not a dry-run
    
    Anonymous users get results from cache which can be outdated for some
    time which can confuse the validation.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- plugins/cmd/uploader/uploader.go
+++ plugins/cmd/uploader/uploader.go
@@ -11,13 +11,16 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/bazelbuild/buildtools/build"
+	"google.golang.org/api/option"
 	"kubevirt.io/project-infra/plugins/mirror"
 )
 
 type options struct {
-	dryRun        bool
-	bucket        string
-	workspacePath string
+	dryRun          bool
+	bucket          string
+	workspacePath   string
+	continueOnError bool
+	verify          bool
 }
 
 func (o *options) Validate() error {
@@ -31,6 +34,8 @@ func gatherOptions() options {
 	o := options{}
 	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	fs.BoolVar(&o.dryRun, "dry-run", true, "Dry run for testing. Uses API tokens but does not mutate.")
+	fs.BoolVar(&o.verify, "verify", false, "Verify that all artifacts are uploaded and that they have the right shasum")
+	fs.BoolVar(&o.continueOnError, "continue-on-error", false, "Try to upload as many artifacts as possible. Exit code will still be non-zero in case of errors")
 	fs.StringVar(&o.bucket, "bucket", "builddeps", "bucket where to upload")
 	fs.StringVar(&o.workspacePath, "workspace", "", "path to the workspace file")
 	fs.Parse(os.Args[1:])
@@ -44,12 +49,6 @@ func main() {
 	}
 	fmt.Println(options.dryRun)
 
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create new storage client: %v.\n", err)
-	}
-
 	rawFile, err := ioutil.ReadFile(options.workspacePath)
 	workspace, err := build.ParseWorkspace("workspace", rawFile)
 	if err != nil {
@@ -59,13 +58,62 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not read artifacts: %v", err)
 	}
+
+	if options.verify {
+		verify(options, artifacts)
+	} else {
+		upload(options, workspace, artifacts)
+	}
+}
+
+func verify(options options, artifacts []mirror.Artifact) {
+	ctx := context.Background()
+	var opts []option.ClientOption
+	if options.dryRun {
+		opts = append(opts, option.WithoutAuthentication())
+	}
+	client, err := storage.NewClient(ctx, opts...)
+	if err != nil {
+		log.Fatalf("Failed to create new storage client: %v.\n", err)
+	}
+	failed := false
+	for _, artifact := range artifacts {
+		newFileUrl := mirror.GenerateFilePath(options.bucket, &artifact)
+		err := mirror.VerifyArtifact(ctx, client, artifact, options.bucket)
+		if err != nil {
+			log.Printf("failed to upload %s to %s: %s", artifact.Name(), newFileUrl, err)
+			failed = true
+		}
+	}
+
+	if failed {
+		os.Exit(1)
+	}
+}
+
+func upload(options options, workspace *build.File, artifacts []mirror.Artifact) {
+	ctx := context.Background()
+	var opts []option.ClientOption
+	if options.dryRun {
+		opts = append(opts, option.WithoutAuthentication())
+	}
+	client, err := storage.NewClient(ctx, opts...)
+	if err != nil {
+		log.Fatalf("Failed to create new storage client: %v.\n", err)
+	}
 	invalid := mirror.FilterArtifactsWithoutMirror(artifacts, regexp.MustCompile(`^https://storage.googleapis.com/.+`))
 
+	failed := false
 	for _, artifact := range invalid {
 		newFileUrl := mirror.GenerateFilePath(options.bucket, &artifact)
 		err := mirror.WriteToBucket(options.dryRun, ctx, client, artifact, options.bucket)
 		if err != nil {
-			log.Fatalf("failed to upload %s to %s: %s", artifact.Name(), newFileUrl, err)
+			log.Printf("failed to upload %s to %s: %s", artifact.Name(), newFileUrl, err)
+			if options.continueOnError {
+				failed = true
+			} else {
+				os.Exit(1)
+			}
 		}
 		artifact.AppendURL(newFileUrl)
 	}
@@ -73,5 +121,9 @@ func main() {
 	err = mirror.WriteWorkspace(options.dryRun, workspace, options.workspacePath)
 	if err != nil {
 		log.Fatalf("could not write workspace file: %v", err)
+	}
+
+	if failed {
+		os.Exit(1)
 	}
 }
--- plugins/mirror/bazel.go
+++ plugins/mirror/bazel.go
@@ -2,12 +2,16 @@ package mirror
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"hash"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
 	"regexp"
 
@@ -70,21 +74,23 @@ func GetArtifacts(workspace *build.File) (artifacts []Artifact, err error) {
 
 func FilterArtifactsWithoutMirror(artifacts []Artifact, regexp *regexp.Regexp) (noMirror []Artifact) {
 	for _, artifact := range artifacts {
-		var mirror string
 		if len(artifact.URLs()) == 0 {
 			continue
 		}
-		for _, url := range artifact.URLs() {
-			if regexp.MatchString(url) {
-				mirror = url
-				break
-			}
-		}
-		if mirror == "" {
+		if mirror := getMirror(artifact, regexp); mirror == "" {
 			noMirror = append(noMirror, artifact)
 		}
 	}
 	return noMirror
+}
+
+func getMirror(artifact Artifact, regexp *regexp.Regexp) string {
+	for _, urlStr := range artifact.URLs() {
+		if regexp.MatchString(urlStr) {
+			return urlStr
+		}
+	}
+	return ""
 }
 
 func WriteToBucket(dryRun bool, ctx context.Context, client *storage.Client, artifact Artifact, bucket string) (err error) {
@@ -105,27 +111,66 @@ func WriteToBucket(dryRun bool, ctx context.Context, client *storage.Client, art
 			continue
 		}
 		defer resp.Body.Close()
+		if resp.StatusCode < 200 || resp.StatusCode > 299 {
+			log.Printf("Could not upload artifact from %s, continuing with next URL: Status Code: %v", uri, resp.StatusCode)
+			continue
+		}
 
 		log.Printf("File will be written to gs://%s/%s", bucket, reportObject.ObjectName())
 
+		var reportOutputWriter io.WriteCloser
 		if dryRun {
-			return nil
-		}
-		reportOutputWriter := reportObject.NewWriter(ctx)
-		defer reportOutputWriter.Close()
-		_, err = io.Copy(reportOutputWriter, resp.Body)
-		if err == nil {
-			return nil
+			reportOutputWriter, err = os.OpenFile("/dev/null", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				return fmt.Errorf("Failed to open /dev/null: %v", err)
+			}
 		} else {
+			reportOutputWriter = reportObject.NewWriter(ctx)
+		}
+		defer reportOutputWriter.Close()
+
+		sha := sha256.New()
+		body := io.TeeReader(resp.Body, sha)
+		_, err = io.Copy(reportOutputWriter, body)
+		if err != nil {
 			log.Printf("Could not upload artifact from %s, continuing with next URL: %v", uri, err)
 			continue
 		}
+		if toHex(sha) != artifact.SHA256() {
+			log.Printf("Could not upload artifact from %s, continuing with next URL: Expected shasum %v, got %v", uri, artifact.SHA256(), toHex(sha))
+			continue
+		}
+		return nil
 	}
 	return fmt.Errorf("artifact download urls exhausted, failed to upload %s", artifact.Name())
+}
+
+func VerifyArtifact(ctx context.Context, client *storage.Client, artifact Artifact, bucket string) (err error) {
+	reportObject := client.Bucket(bucket).Object(artifact.SHA256())
+	reader, err := reportObject.NewReader(ctx)
+	if err == storage.ErrObjectNotExist {
+		return fmt.Errorf("artifact %v: object is not cached: %v", artifact.Name(), err)
+	} else if err != nil {
+		return fmt.Errorf("artifact %v: error checking if object exists: %v", artifact.Name(), err)
+	}
+	defer reader.Close()
+	sha := sha256.New()
+	if _, err := io.Copy(sha, reader); err != nil {
+		return fmt.Errorf("artifact %v: failed to download the object: %v", artifact.Name(), err)
+
+	}
+	if toHex(sha) != artifact.SHA256() {
+		return fmt.Errorf("artifact %v: expected shasum %v, got %v", artifact.Name(), artifact.SHA256(), toHex(sha))
+	}
+	return nil
 }
 
 func GenerateFilePath(bucket string, artifact *Artifact) string {
 	u, _ := url.Parse("https://storage.googleapis.com")
 	u.Path = path.Join(u.Path, bucket, artifact.SHA256())
 	return u.String()
+}
+
+func toHex(hasher hash.Hash) string {
+	return hex.EncodeToString(hasher.Sum(nil))
 }
