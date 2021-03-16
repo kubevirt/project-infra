2bf5e998e88baeee8f841091d52cbe69430cd5c3
Federico Gimenez <fgimenez@users.noreply.github.com>
2021-03-16 09:59:34 +0100

    Prow workloads control plane (#1025)
    * Initial import from kubespray
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * configure calico to use centos8 iptables-nft
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * use cri-o as container runtime
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * removed file that will come from secrets
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * added prow job
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * Removed unused kubespray files
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * use containerd as container runtime
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * fixed for containerd
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * adjusted to latest version supported in kubespray
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * Update github/ci/prow-workloads/hack/deploy.sh
    
    Co-authored-by: Daniel Hiller <daniel.hiller.1972@googlemail.com>
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    Co-authored-by: Daniel Hiller <daniel.hiller.1972@googlemail.com>

---
+++ github/ci/prow-workloads/hack/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+main(){
+    (
+        PROJECT_PATH=$(realpath $(dirname "$0")/../../../..)
+
+        cd ${PROJECT_PATH}
+
+        base_dir=github/ci/prow-workloads
+
+        source ./hack/manage-secrets.sh
+        decrypt_secrets
+        # ssh key
+        mkdir -p ~/.ssh && chmod 0700 ~/.ssh
+        extract_secret 'prowWorkloads.sshKey' ~/.ssh/id_ed25519
+        chmod 0600 ~/.ssh/id_ed25519
+        # ansible config files
+        extract_secret 'prowWorkloads.hostsYml' ${base_dir}/inventory/prow-workloads/hosts.yml
+        extract_secret 'prowWorkloads.groupVarsAllYml' ${base_dir}/inventory/prow-workloads/group_vars/all/all.yml
+
+        cd ${base_dir}
+
+        ansible-playbook -i inventory/prow-workloads/hosts.yaml  --become --become-user=root --private-key ~/.ssh/id_ed25519 cluster.yml
+    )
+}
+
+main "$@"
