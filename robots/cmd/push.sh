6726360d7773fda4bccf09dc4d0f5adce77d8e94
Daniel Hiller <dhiller@redhat.com>
2021-03-17 11:21:18 +0100

    Use stamping for container image tags
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- robots/cmd/push.sh
+++ robots/cmd/push.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+script_dirname=$(cd "$(dirname $0)" && pwd)
+source "$script_dirname/../../hack/print-workspace-status.sh"
+
 image_name="$1"
 
-get_image_tag() {
-    local current_commit today
-    current_commit="$(git rev-parse HEAD)"
-    today="$(date +%Y%m%d)"
-    echo "v${today}-${current_commit:0:7}"
-}
-
-image_tag="$(get_image_tag)"
-bazel run --define container_tag="${image_tag}" "//robots/cmd/${image_name}:push"
-docker pull "quay.io/kubevirtci/${image_name}:${image_tag}"
-docker tag "quay.io/kubevirtci/${image_name}:${image_tag}" "quay.io/kubevirtci/${image_name}:latest"
+bazel run "//robots/cmd/${image_name}:push"
+docker pull "quay.io/kubevirtci/${image_name}:${docker_tag}"
+docker tag "quay.io/kubevirtci/${image_name}:${docker_tag}" "quay.io/kubevirtci/${image_name}:latest"
 docker push "quay.io/kubevirtci/${image_name}:latest"
-bash -x ../../../hack/update-jobs-with-latest-image.sh "quay.io/kubevirtci/${image_name}"
+bash -x "$script_dirname/../../hack/update-jobs-with-latest-image.sh" "quay.io/kubevirtci/${image_name}"
