b66b42ea9ad707a07fbc413de0dfdcb1397eba7e
oscollabus <oshoval@redhat.com>
2021-03-31 17:36:55 +0300

    Infra, Map docker.io images to quay (#1099)
    * infra, Update bootstrap to point to quay
    
    Update docker.io/kubevirtci/kubevirt-infra-bootstrap:v20201201-08dc4a9
    to point to its counterpart
    quay.io/kubevirtci/kubevirt-infra-bootstrap:v20201201-08dc4a9
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update bootstrap to point to quay
    
    Update kubevirtci/bootstrap:v20201119-a5880e0
    to point to its counterpart
    quay.io/kubevirtci/bootstrap:v20201119-a5880e0
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update bootstrap to point to quay
    
    Update kubevirtci/bootstrap:v20210112-b29dfd7
    to point to its counterpart
    quay.io/kubevirtci/bootstrap:v20210112-b29dfd7
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update bootstrap to point to quay
    
    Update kubevirtci/bootstrap:v20210126-a12b6c0
    to point to its counterpart
    quay.io/kubevirtci/bootstrap:v20210126-a12b6c0
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update indexpagecreator to point to quay.io
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update BUILD.bazel files to point to quay
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update kubevirtci/release-blocker related
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * infra, Update kubevirt/builder images to point to quay
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- robots/cmd/indexpagecreator/update-jobs-with-latest-indexpagecreator-image.sh
+++ robots/cmd/indexpagecreator/update-jobs-with-latest-indexpagecreator-image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-docker pull kubevirtci/indexpagecreator
-sha_id=$(docker images --digests kubevirtci/indexpagecreator | grep 'latest ' | head -1 | awk '{ print $3 }')
+docker pull quay.io/kubevirtci/indexpagecreator
+sha_id=$(docker images --digests quay.io/kubevirtci/indexpagecreator | grep 'latest ' | head -1 | awk '{ print $3 }')
 
 for file in $(grep -l 'image: .*/kubevirtci/indexpagecreator' ../../github/ci/prow/files/jobs/**/**/*-periodics.yaml); do
-    sed -i -E 's#index.docker.io/kubevirtci/indexpagecreator@sha256:[a-z0-9]+#index.docker.io/kubevirtci/indexpagecreator@'"$sha_id"'#g' \
+    sed -i -E 's#quay.io/kubevirtci/indexpagecreator@sha256:[a-z0-9]+#quay.io/kubevirtci/indexpagecreator@'"$sha_id"'#g' \
         "$file"
 done
