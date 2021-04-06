4c2c66863980e0c9c5b3f95bf2690adcc34c90bf
Federico Gimenez <fgimenez@redhat.com>
2021-04-06 19:16:58 +0200

    fix git ask pass
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

---
+++ github/ci/prow-deploy/hack/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+main(){
+    current_dir=$(dirname "$0")
+    project_infra_root=$(readlink -f "${current_dir}/../../../..")
+
+    base_dir=${project_infra_root}/github/ci/prow-deploy
+
+    source ${project_infra_root}/hack/manage-secrets.sh
+    decrypt_secrets
+
+    mkdir -p ${base_dir}/vars/${DEPLOY_ENVIRONMENT}
+    mv main.yml ${base_dir}/vars/${DEPLOY_ENVIRONMENT}/secrets.yml
+
+    # run playbook
+    cd ${base_dir}
+    cat << EOF > inventory
+[local]
+localhost ansible_connection=local
+EOF
+    ANSIBLE_ROLES_PATH=$(pwd)/.. ansible-playbook -i inventory prow-deploy.yaml
+}
+
+main "${@}"
