5a035008d87650d0d2cb36bc47458c5bfa1fc29e
Federico Gimenez <fgimenez@users.noreply.github.com>
2021-05-17 13:20:29 +0200

    Adapt TestGrid config managment to Config Merger (#1221)
    * Adapt TestGrid config managment to Config Merger
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * Update github/ci/prow-deploy/files/jobs/kubevirt/project-infra/project-infra-postsubmits.yaml
    
    Co-authored-by: Daniel Hiller <daniel.hiller.1972@googlemail.com>
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    Co-authored-by: Daniel Hiller <daniel.hiller.1972@googlemail.com>

---
+++ github/ci/testgrid/hack/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${BASEDIR}/common.sh
+
+main(){
+    generate_config "${@}"
+
+    if git diff --cached --quiet --exit-code; then
+        echo "No changes in testgrid config. Aborting no-op bump"
+        exit 0
+    fi
+
+    run_tests "${@}"
+}
+
+main "${@}"
