3b4926145c21709377c0fe28b7781e9fa7400205
Federico Gimenez <fgimenez@users.noreply.github.com>
2021-06-08 16:13:32 +0200

    Prow workloads bootstrap automation (#1252)
    * prow-workloads node bootstrap role and playbook
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * added job for bootstrapping nodes
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>
    
    * fixed mirrors update and added reboot
    
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- github/ci/prow-workloads/hack/deploy.sh
+++ github/ci/prow-workloads/hack/deploy.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
+source $(dirname "$0")/common.sh
+
 main(){
-    (
-        PROJECT_PATH=$(realpath $(dirname "$0")/../../../..)
+    setup
 
-        cd ${PROJECT_PATH}
+    cd ${BASE_DIR}
 
-        base_dir=github/ci/prow-workloads
-
-        source ./hack/manage-secrets.sh
-        decrypt_secrets
-        # ssh key
-        mkdir -p ~/.ssh && chmod 0700 ~/.ssh
-        extract_secret 'prowWorkloads.sshKey' ~/.ssh/id_ed25519
-        chmod 0600 ~/.ssh/id_ed25519
-        # ansible config files
-        extract_secret 'prowWorkloads.hostsYml' ${base_dir}/inventory/prow-workloads/hosts.yml
-        extract_secret 'prowWorkloads.groupVarsAllYml' ${base_dir}/inventory/prow-workloads/group_vars/all/all.yml
-
-        cd ${base_dir}
-
-        ansible-playbook -i inventory/prow-workloads/hosts.yaml  --become --become-user=root --private-key ~/.ssh/id_ed25519 cluster.yml
-    )
+    ansible-playbook -i inventory/prow-workloads/hosts.yaml  --become --become-user=root --private-key ~/.ssh/id_ed25519 /kubespray/cluster.yml
 }
 
 main "$@"
