b412c95c1dc971a4c1882f3eac4fa610ac3a1a8e
Daniel Hiller <daniel.hiller.1972@gmail.com>
2021-09-22 15:32:31 +0200

    Add new job and script that bump images used in prow job definitions (#1581)
    * Add new job and script that bump images
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Update test-infra to latest commit to fix build
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Fix hold label for labels-checker
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Increase memory and cpu for pr-creator image builds, add exit to bump
    
    Increase the memory and cpu of the jobs that build and push the
    pr-creator image to avoid the jobs getting OOMKilled.
    
    Also exit the image bump with non zero exit code in case of error.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ hack/bump-prow-job-images.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_INFRA_ROOT=$(readlink --canonicalize ${BASEDIR}/..)
+
+# won't work as image names differ from directory names
+#IMAGES=$(
+#    ( cd "$PROJECT_INFRA_ROOT/images" && find . -mindepth 1 -maxdepth 1 -type d -print ) & ( cd "$PROJECT_INFRA_ROOT/robots/cmd" && find . -mindepth 1 -maxdepth 1 -type d -print ) ;
+#)
+IMAGES=( autoowners bootstrap ci-usage-exporter flakefinder golang indexpagecreator kubekins-e2e kubevirt-infra-bootstrap kubevirt-userguide prow-deploy pr-creator release-blocker release-querier )
+for image_dir in "${IMAGES[@]}"; do
+    image_name="quay.io/kubevirtci/${image_dir/#\.\//}"
+    if ! "$PROJECT_INFRA_ROOT/hack/update-jobs-with-latest-image.sh" "$image_name"; then
+        echo "Failed to update prow jobs using image $image_name"
+        exit 1
+    fi
+    echo "Updated prow jobs using image $image_name"
+done
