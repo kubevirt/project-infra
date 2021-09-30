0ae9368e98e4de52be5a385c6111b42815f48313
Marcelo Carneiro do Amaral <marcelo.amaral1@ibm.com>
2021-09-30 21:56:19 +0900

    add the config files to create the sig-scale cluster (#1629)
    Signed-off-by: Marcelo Amaral <marcelo.amaral1@ibm.com>

---
+++ github/ci/prow-performance-workloads/hack/common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_PATH=$(realpath $(dirname "$0")/../../../..)
+BASE_DIR=${PROJECT_PATH}/github/ci/prow-performance-workloads
+
+setup(){
+    cd ${PROJECT_PATH}
+
+    source ./hack/manage-secrets.sh
+    decrypt_secrets
+    # ssh key
+    mkdir -p ~/.ssh && chmod 0700 ~/.ssh
+    extract_secret 'prowPerformance.sshKey' ~/.ssh/id_rsa
+    chmod 0600 ~/.ssh/id_rsa
+    # ansible config files
+    extract_secret 'prowPerformance.hostsYml' ${BASE_DIR}/inventory/prow-performance-workloads/hosts.yml
+    extract_secret 'prowPerformance.groupVarsAllYml' ${BASE_DIR}/inventory/prow-performance-workloads/group_vars/all/all.yml
+}
