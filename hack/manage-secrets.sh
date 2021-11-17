8331f49ba70ca46a573b2765d401b172da15e247
Federico Gimenez <fgimenez@users.noreply.github.com>
2021-11-17 15:37:44 +0100

    Add yq for managing secrets if not present (#1736)
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

--- hack/manage-secrets.sh
+++ hack/manage-secrets.sh
@@ -19,6 +19,12 @@ extract_secret(){
     local key="${1}"
     local path="${2}"
 
+    if ! command -v yq >/dev/null 2>&1; then
+        curl -Lo ./yq https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 && \
+            chmod a+x ./yq && \
+            mv ./yq /usr/local/bin
+    fi
+
     mkdir -p $(dirname "${path}")
     # only remove new line at the end
     yq r main.yml "${key}" | awk 'NR>1{print PREV} {PREV=$0} END{printf("%s",$0)}' > "${path}"
