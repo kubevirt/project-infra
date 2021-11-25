cf2a1e5c47772ece0fd7fd8626878b0ed8bebd8c
Marcelo Amaral <marcelo.amaral1@ibm.com>
2021-11-25 16:30:16 +0900

    update kubespray context to enable ansible-playbook find the right inventory configuration
    Signed-off-by: Marcelo Amaral <marcelo.amaral1@ibm.com>

--- github/ci/prow-performance-workloads/hack/deploy.sh
+++ github/ci/prow-performance-workloads/hack/deploy.sh
@@ -5,9 +5,11 @@ source $(dirname "$0")/common.sh
 main(){
     setup
 
-    cd ${BASE_DIR}
+    cd /kubespray
+    
+    cp -r ${BASE_DIR}/inventory/prow-performance-workloads inventory/
 
-    ansible-playbook -i inventory/prow-performance-workloads/hosts.yml  --become --become-user=root --private-key ~/.ssh/id_rsa /kubespray/cluster.yml
+    ansible-playbook -i inventory/prow-performance-workloads/hosts.yml  --become --become-user=root --private-key ~/.ssh/id_rsa cluster.yml
 }
 
 main "$@"
