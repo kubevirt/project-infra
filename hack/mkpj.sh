8658d1da672d47375190fb372af6244f617313cb
Federico Gimenez <fgimenez@redhat.com>
2021-12-01 15:01:40 +0100

    aAdd mkpj.sh script
    Signed-off-by: Federico Gimenez <fgimenez@redhat.com>

---
+++ hack/mkpj.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Adapted from https://github.com/kubernetes/test-infra/blob/master/config/mkpj.sh
+#
+# Usage: hack/mkpj.sh --job <job-name> | kubectl -n kubevirt-prow-jobs -f
+#
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+config="${root}/github/ci/prow-deploy/files/config.yaml"
+job_config_path="${root}/github/ci/prow-deploy/files/jobs"
+
+docker pull gcr.io/k8s-prow/mkpj 1>&2 || true
+docker run -i --rm -v "${root}:${root}:z" gcr.io/k8s-prow/mkpj "--config-path=${config}" "--job-config-path=${job_config_path}" "$@"
