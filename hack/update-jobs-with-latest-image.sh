27cc5be25089d64f3d5196c31fc56b1ccd84c0c3
Daniel Hiller <daniel.hiller.1972@gmail.com>
2022-01-17 11:43:22 +0100

    Remove 'latest' tag from list of image tags (#1856)
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/update-jobs-with-latest-image.sh
+++ hack/update-jobs-with-latest-image.sh
@@ -8,7 +8,7 @@ if ! command -V skopeo; then
 fi
 
 IMAGE_NAME="$1"
-latest_image_tag=$(skopeo list-tags "docker://$IMAGE_NAME" | jq -r '.Tags[]' | tail -1)
+latest_image_tag=$(skopeo list-tags "docker://$IMAGE_NAME" | jq -r '.Tags[] | select( contains("latest") | not )' | tail -1)
 if [ -z "$latest_image_tag" ]; then
     echo "Couldn't find latest_image_tag"
     exit 1
@@ -17,6 +17,6 @@ IMAGE_NAME_WITH_TAG="$IMAGE_NAME:$latest_image_tag"
 
 replace_regex='s#'"$IMAGE_NAME"'(@sha256\:|:v[a-z0-9]+-).*$#'"$IMAGE_NAME_WITH_TAG"'#g'
 
-job_dir="$(readlink --canonicalize "$(cd "$(cd "$(dirname $0)" && pwd)"'/../github/ci/prow-deploy/files/jobs' && pwd)")"
+job_dir="$(readlink --canonicalize "$(cd "$(cd "$(dirname "$0")" && pwd)"'/../github/ci/prow-deploy/files/jobs' && pwd)")"
 
-find "$job_dir" -regextype egrep -regex '.*-(periodics|presubmits|postsubmits)\.yaml' -exec sed -i -E $replace_regex {} +
+find "$job_dir" -regextype egrep -regex '.*-(periodics|presubmits|postsubmits)\.yaml' -exec sed -i -E "$replace_regex" {} +
