c40d39d66d3199753bf6ab45b341cd511b837ab9
Daniel Hiller <dhiller@redhat.com>
2022-02-04 17:02:39 +0100

    Add ./hack/flake-report-creator.sh
    Add script to execute flake-report-creator using docker.
    
    Example:
    
    ```
    ./hack/flake-report-creator.sh jenkins
    ```
    
    Update examples in README.md to cover the script usage.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ hack/flake-report-creator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+tmp_dir="$(mktemp -d)"
+docker run -v /etc/pki:/etc/pki -v /etc/ssl:/etc/ssl \
+        -v "$(dirname $(echo $GOOGLE_APPLICATION_CREDENTIALS)):$(dirname $(echo $GOOGLE_APPLICATION_CREDENTIALS))" \
+        -e GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_APPLICATION_CREDENTIALS" \
+        -v "${tmp_dir}:/tmp:Z" \
+        --network host \
+        quay.io/kubevirtci/flake-report-creator:v20220204-cc4b2a56 \
+        --overwrite --outputFile=/tmp/report.html \
+        "$@"
+
+echo "$tmp_dir/report.html"
