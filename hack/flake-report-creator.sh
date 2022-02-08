dfd80a3edb3f1ae5dc7ef3c67c97f0fb03157e30
Daniel Hiller <dhiller@redhat.com>
2022-02-08 09:25:25 +0100

    Update image in script
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/flake-report-creator.sh
+++ hack/flake-report-creator.sh
@@ -6,7 +6,7 @@ docker run -v /etc/pki:/etc/pki -v /etc/ssl:/etc/ssl \
         -e GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_APPLICATION_CREDENTIALS" \
         -v "${tmp_dir}:/tmp:Z" \
         --network host \
-        quay.io/kubevirtci/flake-report-creator:v20220204-cc4b2a56 \
+        quay.io/kubevirtci/flake-report-creator:v20220208-fc7f17c2 \
         --overwrite --outputFile=/tmp/report.html \
         "$@"
 
