8ea12d6f3c8bb34a5f4d46384ed7f82e9a5bf97e
Daniel Hiller <dhiller@redhat.com>
2022-06-07 15:05:10 +0200

    Fix html to be valid xml
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- robots/cmd/flake-report-creator/cmd/jenkins_test.go
+++ robots/cmd/flake-report-creator/cmd/jenkins_test.go
@@ -1,10 +1,17 @@
 package cmd
 
 import (
+	"encoding/xml"
 	"github.com/bndr/gojenkins"
+	"github.com/joshdk/go-junit"
 	log "github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"kubevirt.io/project-infra/robots/pkg/flakefinder"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
 )
 
 func Test_fetchJunitFilesFromArtifacts(t *testing.T) {
@@ -80,6 +87,191 @@ func Test_fetchJunitFilesFromArtifacts(t *testing.T) {
 			}
 			if !reflect.DeepEqual(tt.want, actualFileNames) {
 				t.Errorf("fetchJunitFilesFromArtifacts() = %v, want %v", actualFileNames, tt.want)
+			}
+		})
+	}
+}
+
+func Test_writeReportToFile(t *testing.T) {
+	type args struct {
+		startOfReport time.Time
+		endOfReport   time.Time
+		reports       []*flakefinder.JobResult
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "one job - failed tests",
+			args: args{
+				startOfReport: time.Now(),
+				endOfReport:   time.Now(),
+				reports: []*flakefinder.JobResult{
+					{
+						Job: "job1",
+						JUnit: []junit.Suite{
+							{
+								Name:       "Suite 1",
+								Package:    "test.blah.package",
+								Properties: nil,
+								Tests: []junit.Test{
+									{
+										Name:       "Test 1",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusFailed,
+										Error:      nil,
+										Properties: nil,
+									},
+									{
+										Name:       "Test 2",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusError,
+										Error:      nil,
+										Properties: nil,
+									},
+								},
+								SystemOut: "",
+								SystemErr: "",
+								Totals: junit.Totals{
+									Tests:    42,
+									Passed:   37,
+									Skipped:  17,
+									Failed:   11,
+									Error:    3,
+									Duration: 1742,
+								},
+							},
+						},
+						BuildNumber: 1,
+						PR:          42,
+					},
+				},
+			},
+		},
+		{
+			name: "two jobs - failed tests",
+			args: args{
+				startOfReport: time.Now(),
+				endOfReport:   time.Now(),
+				reports: []*flakefinder.JobResult{
+					{
+						Job: "job1",
+						JUnit: []junit.Suite{
+							{
+								Name:       "Suite 1",
+								Package:    "test.blah.package",
+								Properties: nil,
+								Tests: []junit.Test{
+									{
+										Name:       "Test 1",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusFailed,
+										Error:      nil,
+										Properties: nil,
+									},
+									{
+										Name:       "Test 2",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusError,
+										Error:      nil,
+										Properties: nil,
+									},
+								},
+								SystemOut: "",
+								SystemErr: "",
+								Totals: junit.Totals{
+									Tests:    42,
+									Passed:   37,
+									Skipped:  17,
+									Failed:   11,
+									Error:    3,
+									Duration: 1742,
+								},
+							},
+						},
+						BuildNumber: 1,
+						PR:          42,
+					},
+					{
+						Job: "job2",
+						JUnit: []junit.Suite{
+							{
+								Name:       "Suite 1",
+								Package:    "test.blah.package",
+								Properties: nil,
+								Tests: []junit.Test{
+									{
+										Name:       "Test 3",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusFailed,
+										Error:      nil,
+										Properties: nil,
+									},
+									{
+										Name:       "Test 4",
+										Classname:  "testClass",
+										Duration:   37,
+										Status:     junit.StatusError,
+										Error:      nil,
+										Properties: nil,
+									},
+								},
+								SystemOut: "",
+								SystemErr: "",
+								Totals: junit.Totals{
+									Tests:    42,
+									Passed:   37,
+									Skipped:  17,
+									Failed:   11,
+									Error:    3,
+									Duration: 1742,
+								},
+							},
+						},
+						BuildNumber: 1,
+						PR:          42,
+					},
+				},
+			},
+		},
+	}
+
+	tempFile, err := ioutil.TempFile("", "reportFile")
+	if err != nil {
+		t.Errorf("failed to create temp report file: %v", err)
+		return
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			writeReportToFile(tt.args.startOfReport, tt.args.endOfReport, tt.args.reports, tempFile.Name())
+
+			// validate output is valid xml
+			file, err := ioutil.ReadFile(tempFile.Name())
+			if err != nil {
+				t.Errorf("failed to read temp report file: %v", err)
+				return
+			}
+			r := strings.NewReader(string(file))
+			d := xml.NewDecoder(r)
+
+			d.Strict = true
+			d.Entity = xml.HTMLEntity
+			for {
+				_, err := d.Token()
+				switch err {
+				case io.EOF:
+					return
+				case nil:
+				default:
+					t.Errorf("Report:\n%s\n\nfailed to validate report file: %v", string(file), err)
+					return
+				}
 			}
 		})
 	}
