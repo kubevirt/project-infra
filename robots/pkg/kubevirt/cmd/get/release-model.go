8b4b531fb7dbf02818879b44e0aeba55893c1673
Daniel Hiller <dhiller@redhat.com>
2023-09-27 11:11:37 +0200

    Generate support matrix for kubevirt/kubevirt (#2948)
    * Support matrix full version
    
    Includes information about which versions are already EOL.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Add job config and overwrite parameters
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Add prow job to update document in sig-release
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * Add --kubevirt-version flag for release page
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    ---------
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ robots/pkg/kubevirt/cmd/get/release-model.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/* Original file: https://raw.githubusercontent.com/kubernetes/release/master/cmd/schedule-builder/cmd/model.go
+ */
+
+package get
+
+// PatchSchedule main struct to hold the schedules
+type PatchSchedule struct {
+	Schedules []Schedule `yaml:"schedules"`
+}
+
+// PreviousPatches struct to define the old patch schedules
+type PreviousPatches struct {
+	Release            string `yaml:"release"`
+	CherryPickDeadline string `yaml:"cherryPickDeadline"`
+	TargetDate         string `yaml:"targetDate"`
+	Note               string `yaml:"note"`
+}
+
+type NextRelease struct {
+	Release            string `yaml:"release"`
+	CherryPickDeadline string `yaml:"cherryPickDeadline"`
+	TargetDate         string `yaml:"targetDate"`
+}
+
+// Schedule struct to define the release schedule for a specific version
+type Schedule struct {
+	Release            string            `yaml:"release"`
+	Next               NextRelease       `yaml:"next"`
+	CherryPickDeadline string            `yaml:"cherryPickDeadline"`
+	TargetDate         string            `yaml:"targetDate"`
+	EndOfLifeDate      string            `yaml:"endOfLifeDate"`
+	PreviousPatches    []PreviousPatches `yaml:"previousPatches"`
+}
+
+type ReleaseSchedule struct {
+	Releases []Release `yaml:"releases"`
+}
+
+type Release struct {
+	Version  string     `yaml:"version"`
+	Timeline []Timeline `yaml:"timeline"`
+}
+
+type Timeline struct {
+	What     string `yaml:"what"`
+	Who      string `yaml:"who"`
+	When     string `yaml:"when"`
+	Week     string `yaml:"week"`
+	CISignal string `yaml:"ciSignal"`
+	Tldr     bool   `yaml:"tldr"`
+}
+
+type EOLDoc struct {
+	Branches []*Branch
+}
+
+type Branch struct {
+	Release           string `yaml:"release"`
+	FinalPatchRelease string `yaml:"finalPatchRelease"`
+	EndOfLifeDate     string `yaml:"endOfLifeDate"`
+	Note              string `yaml:"note"`
+}
