058921ad5b90f9d8ece807a1a5d815ee4dee5ebe
Alay Patel <alay1431@gmail.com>
2023-10-19 08:51:11 -0400

    update k8s version to 1.27 for perf-report-creator job scraping (#3032)
    Signed-off-by: Alay Patel <alayp@nvidia.com>
    Co-authored-by: Alay Patel <alayp@nvidia.com>

--- robots/cmd/perf-report-creator/scrape.sh
+++ robots/cmd/perf-report-creator/scrape.sh
@@ -2,14 +2,14 @@ CREDENTIALS_FILE=${1}
 BENCHMARKS_DIR=${2}
 OUTPUT_DIR=${3:-output}
 
-# scrape sig-performance 1.25 results
-perf-report-creator results --credentials-file=${CREDENTIALS_FILE} --output-dir ${OUTPUT_DIR}/results --since 168h0s
+# scrape sig-performance 1.27 results
+perf-report-creator results --credentials-file=${CREDENTIALS_FILE} --output-dir ${OUTPUT_DIR}/results --since 168h0s --performance-job-name periodic-kubevirt-e2e-k8s-1.27-sig-performance
 # scrape 100 density test results
 perf-report-creator results --credentials-file=${CREDENTIALS_FILE} --output-dir ${OUTPUT_DIR}/results --since 168h0s --performance-job-name periodic-kubevirt-performance-cluster-100-density-test
 
-# aggregate sig-performance 1.25 results in weekly directory
-perf-report-creator weekly-report --output-dir=${OUTPUT_DIR}/weekly/periodic-kubevirt-e2e-k8s-1.25-sig-performance \
-  --results-dir=${OUTPUT_DIR}/results/periodic-kubevirt-e2e-k8s-1.25-sig-performance \
+# aggregate sig-performance 1.27 results in weekly directory
+perf-report-creator weekly-report --output-dir=${OUTPUT_DIR}/weekly/periodic-kubevirt-e2e-k8s-1.27-sig-performance \
+  --results-dir=${OUTPUT_DIR}/results/periodic-kubevirt-e2e-k8s-1.27-sig-performance \
   --vmi-metrics-list=vmiCreationToRunningSecondsP50,vmiCreationToRunningSecondsP95,LIST-virtualmachineinstances-count,LIST-pods-count,LIST-nodes-count,LIST-virtualmachineinstancemigrations-count,LIST-endpoints-count,GET-virtualmachineinstances-count,GET-pods-count,GET-nodes-count,GET-virtualmachineinstancemigrations-count,GET-endpoints-count,CREATE-virtualmachineinstances-count,CREATE-pods-count,CREATE-nodes-count,CREATE-virtualmachineinstancemigrations-count,CREATE-endpoints-count,PATCH-virtualmachineinstances-count,PATCH-pods-count,PATCH-nodes-count,PATCH-virtualmachineinstancemigrations-count,PATCH-endpoints-count,UPDATE-virtualmachineinstances-count,UPDATE-pods-count,UPDATE-nodes-count,UPDATE-virtualmachineinstancemigrations-count,UPDATE-endpoints-count \
   --vm-metrics-list vmiCreationToRunningSecondsP50,vmiCreationToRunningSecondsP95,LIST-virtualmachineinstances-count,LIST-pods-count,LIST-nodes-count,LIST-virtualmachineinstancemigrations-count,LIST-endpoints-count,GET-virtualmachineinstances-count,GET-pods-count,GET-nodes-count,GET-virtualmachineinstancemigrations-count,GET-endpoints-count,CREATE-virtualmachineinstances-count,CREATE-pods-count,CREATE-nodes-count,CREATE-virtualmachineinstancemigrations-count,CREATE-endpoints-count,PATCH-virtualmachineinstances-count,PATCH-pods-count,PATCH-nodes-count,PATCH-virtualmachineinstancemigrations-count,PATCH-endpoints-count,UPDATE-virtualmachineinstances-count,UPDATE-pods-count,UPDATE-nodes-count,UPDATE-virtualmachineinstancemigrations-count,UPDATE-endpoints-count
 
