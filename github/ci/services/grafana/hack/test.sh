610e11b629c6319970e43f4bcd4deb251702b995
Daniel Hiller <dhiller@redhat.com>
2024-01-26 12:01:36 +0100

    grafana,presubmit: Update lane to use kubevirtci
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    Co-authored-by: Brian Carey <bcarey@redhat.com>
    Co-authored-by: Daniel Hiller <dhiller@redhat.com>

--- github/ci/services/grafana/hack/test.sh
+++ github/ci/services/grafana/hack/test.sh
@@ -7,30 +7,18 @@ PROJECT_INFRA_ROOT=$(readlink --canonicalize ${BASEDIR}/../../../../../)
 
 cd $PROJECT_INFRA_ROOT
 
-cat <<EOF | kind create cluster --image quay.io/kubevirtci/kindest-node:v1.27.1 --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-nodes:
-- role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
-  extraPortMappings:
-  - containerPort: 80
-    hostPort: 80
-    protocol: TCP
-  - containerPort: 443
-    hostPort: 443
-    protocol: TCP
-EOF
+KUBEVIRT_DIR=${KUBEVIRTCI_DIR:-/home/prow/go/src/github.com/kubevirt/kubevirt}
+
+cd $KUBEVIRT_DIR && make cluster-up
+
+export KUBECONFIG=$(./cluster-up/kubeconfig.sh)
+
+cd "$PROJECT_INFRA_ROOT"
 
 kubectl create ns monitoring
 
 # Install ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
 
 helm upgrade --namespace monitoring -i grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator --version v5.4.1
 
