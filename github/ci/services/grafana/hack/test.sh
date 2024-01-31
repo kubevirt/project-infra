58f10d0f54a68743043fe4a3da0c2b32a3664995
Brian Carey <bcarey@redhat.com>
2024-01-31 12:10:16 +0000

    grafana,presubmit: Do not deploy ingress in grafana test env
    The e2e tests as part of this lane do not require an ingress deployed
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>

--- github/ci/services/grafana/hack/test.sh
+++ github/ci/services/grafana/hack/test.sh
@@ -7,7 +7,7 @@ PROJECT_INFRA_ROOT=$(readlink --canonicalize ${BASEDIR}/../../../../../)
 
 cd $PROJECT_INFRA_ROOT
 
-KUBEVIRT_DIR=${KUBEVIRTCI_DIR:-/home/prow/go/src/github.com/kubevirt/kubevirt}
+KUBEVIRT_DIR=${KUBEVIRT_DIR:-/home/prow/go/src/github.com/kubevirt/kubevirt}
 
 cd $KUBEVIRT_DIR && make cluster-up
 
@@ -17,14 +17,11 @@ cd "$PROJECT_INFRA_ROOT"
 
 kubectl create ns monitoring
 
-# Install ingress
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
-
 helm upgrade --namespace monitoring -i grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator --version v5.4.1
 
 ./github/ci/services/grafana/hack/deploy.sh testing
 
 kubectl label -n monitoring svc/grafana-service app=grafana
 
-bazelisk test //github/ci/services/grafana/e2e:go_default_test --test_output=all --test_arg=-test.v
+bazelisk test //github/ci/services/grafana/e2e:go_default_test --test_env=KUBECONFIG=$KUBECONFIG --test_output=all --test_arg=-test.v
 
