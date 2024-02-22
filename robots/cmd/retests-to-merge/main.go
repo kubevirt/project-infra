e7ceaec4253242e00559fc570229ad04d783472c
Daniel Hiller <dhiller@redhat.com>
2024-02-22 13:16:16 +0100

    robots, retests-to-merge: add README, refine output
    Refines the output of the cli tool to sort the output starting with
    highest number of retest PRs first. Adds README and updates main README
    to contain entry for tool.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- robots/cmd/retests-to-merge/main.go
+++ robots/cmd/retests-to-merge/main.go
@@ -22,7 +22,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/shurcooL/githubv4"
@@ -32,14 +34,28 @@ import (
 	"kubevirt.io/project-infra/external-plugins/referee/ghgraphql"
 )
 
+type result struct {
+	ghgraphql.PullRequest
+	ghgraphql.PRTimelineForLastCommit
+	ghgraphql.PRLabels
+}
+
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
 func main() {
-	var tokenPath string
+	var tokenPath, org, repo string
 	flag.StringVar(&tokenPath, "github-token-path", "", "the path to the GitHub token to use")
+	flag.StringVar(&org, "org", "kubevirt", "the path to the GitHub token to use")
+	flag.StringVar(&repo, "repo", "kubevirt", "the path to the GitHub token to use")
 	flag.Parse()
+
+	log := logrus.WithField("robot", "retests-to-merge").WithField("repo", fmt.Sprintf("%s/%s", org, repo))
 
 	token, err := os.ReadFile(tokenPath)
 	if err != nil {
-		logrus.Fatalf("failed to use github token path %s: %v", tokenPath, err)
+		log.Fatalf("failed to use github token path %s: %v", tokenPath, err)
 	}
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: strings.TrimSpace(string(token))},
@@ -49,29 +65,52 @@ func main() {
 	var gitHubClient *githubv4.Client
 	gitHubClient = githubv4.NewClient(httpClient)
 
-	org := "kubevirt"
-	repo := "kubevirt"
-
 	gitHubGraphQLClient := ghgraphql.NewClient(gitHubClient)
 
 	pullRequests, err := gitHubGraphQLClient.FetchOpenApprovedAndLGTMedPRs(org, repo)
 	if err != nil {
-		logrus.Fatalf("failed to fetch pull requests for %s/%s: %v", org, repo, err)
+		log.Fatalf("failed to fetch pull requests for %s/%s: %v", org, repo, err)
 	}
 
+	numRetestsToPRNumsToResults := make(map[int]map[int]result)
+	numRetests := make(map[int]struct{})
+	log.Infof("checking %d PRs for retests", len(pullRequests.PRs))
 	for _, pr := range pullRequests.PRs {
+		prLog := log.WithField("pr_url", fmt.Sprintf("https://github.com/%s/%s/pull/%d", org, repo, pr.Number)).WithField("pr_title", pr.Title)
 		prTimeLineForLastCommit, err := gitHubGraphQLClient.FetchPRTimeLineForLastCommit(org, repo, pr.Number)
 		if err != nil {
-			logrus.Fatalf("failed to fetch number of retest comments for pr %s/%s#%d: %v", org, repo, pr.Number, err)
+			prLog.Fatalf("failed to fetch number of retest comments for pr %s/%s#%d: %v", org, repo, pr.Number, err)
 		}
-		if prTimeLineForLastCommit.NumberOfRetestComments < 5 {
+		if prTimeLineForLastCommit.NumberOfRetestComments <= 0 {
 			continue
 		}
-		logrus.Infof("https://github.com/%s/%s/pull/%d - pr %q has %d retests", org, repo, pr.Number, pr.Title, prTimeLineForLastCommit.NumberOfRetestComments)
+		numRetests[prTimeLineForLastCommit.NumberOfRetestComments] = struct{}{}
+		if _, ok := numRetestsToPRNumsToResults[prTimeLineForLastCommit.NumberOfRetestComments]; !ok {
+			numRetestsToPRNumsToResults[prTimeLineForLastCommit.NumberOfRetestComments] = make(map[int]result)
+		}
 		labels, err := gitHubGraphQLClient.FetchPRLabels(org, repo, pr.Number)
 		if err != nil {
-			logrus.Fatalf("failed to fetch number of retest comments for pr %s/%s#%d: %v", org, repo, pr.Number, err)
+			prLog.Fatalf("failed to fetch number of retest comments: %v", err)
 		}
-		logrus.Infof("labels for PR: %+v", labels)
+		numRetestsToPRNumsToResults[prTimeLineForLastCommit.NumberOfRetestComments][pr.Number] = result{
+			PullRequest:             pr,
+			PRTimelineForLastCommit: prTimeLineForLastCommit,
+			PRLabels:                labels,
+		}
+	}
+	if len(numRetests) == 0 {
+		log.Infof("no excessive retests found - hooray")
+	}
+	numRetestsSlice := make([]int, len(numRetests))
+	for numRetest := range numRetests {
+		numRetestsSlice = append(numRetestsSlice, numRetest)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(numRetestsSlice)))
+	log.Infof("max number of retests encountered: %d", numRetestsSlice[0])
+	for _, numRetest := range numRetestsSlice {
+		numRetestLog := log.WithField("num_retests", numRetest)
+		for _, results := range numRetestsToPRNumsToResults[numRetest] {
+			numRetestLog.WithField("pr_url", fmt.Sprintf("https://github.com/%s/%s/pull/%d", org, repo, results.PullRequest.Number)).WithField("pr_title", results.PullRequest.Title).Infof("labels: %v", results.PRLabels)
+		}
 	}
 }
