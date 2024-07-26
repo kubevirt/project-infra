9f9b296da2cc89dcb0d0906ae7dab41062d904a6
Daniel Hiller <dhiller@redhat.com>
2024-07-26 09:20:37 +0200

    prow, referee: enable pull_request events, fix switch (#3550)
    The referee plugin didn't react to pull_request events, since it was not
    enabled to do so in the plugin configuration.
    
    Also switch in go works differently - there's no fallthru. This is fixed
    with this change.
    
    Finally we add some more cases to update the metrics, i.e. converted to
    draft vs. ready for review.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- external-plugins/referee/server/server.go
+++ external-plugins/referee/server/server.go
@@ -149,31 +149,39 @@ func (s *Server) handlePREvent(pr github.PullRequestEvent) error {
 	log := s.Log.WithField("pull_request_url", pullRequestURL)
 
 	switch action {
-	case github.PullRequestActionOpened:
-	case github.PullRequestActionReopened:
-	case github.PullRequestActionSynchronize:
-		// update metrics for PRs
-		prTimeLineForLastCommit, err := s.GHGraphQLClient.FetchPRTimeLineForLastCommit(org, repo, num)
-		if err != nil {
-			return fmt.Errorf("%s - failed to fetch number of retest comments: %w", pullRequestURL, err)
-		}
-		switch prTimeLineForLastCommit.NumberOfRetestComments {
-		case 0:
-			metrics.DeleteForPullRequest(org, repo, num)
-			break
-		default:
-			metrics.SetForPullRequest(org, repo, num, prTimeLineForLastCommit.NumberOfRetestComments)
-		}
-		log.Infof("updated metrics on PR")
-		break
+	case github.PullRequestActionConvertedToDraft:
+		// PRs that have been converted to draft are not tested thus they are not of interest for metrics
+		metrics.DeleteForPullRequest(org, repo, num)
+		return nil
 	case github.PullRequestActionClosed:
 		// PRs that have been merged or closed are not of interest for metrics
 		metrics.DeleteForPullRequest(org, repo, num)
-		break
+		return nil
+	case github.PullRequestActionOpened:
+	case github.PullRequestActionReopened:
+	case github.PullRequestActionReadyForReview:
+	case github.PullRequestActionSynchronize:
+		// the above cases are the ones where we need to update the metrics
 	default:
+		// all other cases -> no metric action necessary
 		log.Infof("skipping pull_request event action %s", action)
 		return nil
 	}
+	return updateMetricsForPRs(s, org, repo, num, pullRequestURL, log)
+}
+
+func updateMetricsForPRs(s *Server, org string, repo string, num int, pullRequestURL string, log *logrus.Entry) error {
+	prTimeLineForLastCommit, err := s.GHGraphQLClient.FetchPRTimeLineForLastCommit(org, repo, num)
+	if err != nil {
+		return fmt.Errorf("%s - failed to fetch number of retest comments: %w", pullRequestURL, err)
+	}
+	switch prTimeLineForLastCommit.NumberOfRetestComments {
+	case 0:
+		metrics.DeleteForPullRequest(org, repo, num)
+	default:
+		metrics.SetForPullRequest(org, repo, num, prTimeLineForLastCommit.NumberOfRetestComments)
+	}
+	log.Infof("updated metrics on PR")
 	return nil
 }
 
