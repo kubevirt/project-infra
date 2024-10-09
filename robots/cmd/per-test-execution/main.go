d724419306896af5a713feb81b12bd14de7c1ff2
Daniel Hiller <dhiller@redhat.com>
2024-10-09 15:19:02 +0200

    add config-file flag, parallelize data retrieval
    Adds flag config-file to replace default config with external config
    file.
    Data retrieval is running in one process per lane now.
    Fixed file write being incomplete.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- robots/cmd/per-test-execution/main.go
+++ robots/cmd/per-test-execution/main.go
@@ -34,6 +34,7 @@ import (
 	"sigs.k8s.io/yaml"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -47,7 +48,7 @@ var (
 	opts options
 
 	//go:embed config.yaml
-	configContent []byte
+	defaultConfig []byte
 
 	k8sVersionRegex = regexp.MustCompile(`^[0-9]\.[1-9][0-9]+$`)
 )
@@ -57,7 +58,7 @@ type Config struct {
 }
 
 func init() {
-	err := yaml.Unmarshal(configContent, &config)
+	err := yaml.Unmarshal(defaultConfig, &config)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,6 +67,7 @@ func init() {
 type options struct {
 	Months     int
 	K8sVersion string
+	ConfigPath string
 }
 
 func (o options) validate() error {
@@ -75,12 +77,32 @@ func (o options) validate() error {
 	if !k8sVersionRegex.MatchString(o.K8sVersion) {
 		return fmt.Errorf("invalid k8s version")
 	}
+	if o.ConfigPath != "" {
+		providedConfig, err := os.ReadFile(o.ConfigPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = yaml.Unmarshal(providedConfig, &config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Infof("applied configuration from %q\n%s", o.ConfigPath, string(providedConfig))
+	} else {
+		log.Infof("using default configuration\n%s", string(defaultConfig))
+	}
 	return nil
 }
 
+type testExecutions struct {
+	Name             string
+	TotalExecutions  int
+	FailedExecutions int
+}
+
 func main() {
-	flag.IntVar(&opts.Months, "months", 1, "determines how much days in the past till today are covered")
+	flag.IntVar(&opts.Months, "months", 1, "determines how much months in the past till today are covered")
 	flag.StringVar(&opts.K8sVersion, "kubernetes-version", "1.31", "string defining the k8s version that has the most recent lane")
+	flag.StringVar(&opts.ConfigPath, "config-path", "", "path to the config file")
 	flag.Parse()
 
 	err := opts.validate()
@@ -94,81 +116,89 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	startOfReport, err := time.Parse(time.DateOnly, fmt.Sprintf("%04d-%02d-01", currentYear, int(currentMonth)-opts.Months))
-	//startOfReport := endOfReport.Add(-7 * 24 * time.Hour)
+	startOfReport := endOfReport.AddDate(0, -opts.Months, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Infof("Running reports for %s - %s", startOfReport.Format(time.DateOnly), endOfReport.Format(time.DateOnly))
+
 	jobDir := "logs"
 	ctx := context.TODO()
 	storageClient, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var wg sync.WaitGroup
+	wg.Add(len(config.Lanes))
 	for _, periodicJobDirPattern := range config.Lanes {
 		periodicJobDir := fmt.Sprintf(periodicJobDirPattern, opts.K8sVersion)
-		results, err := flakefinder.FindUnitTestFilesForPeriodicJob(ctx, storageClient, BucketName, []string{jobDir, periodicJobDir}, startOfReport, endOfReport)
-		if err != nil {
-			log.Printf("failed to load periodicJobDirs for %v: %v", fmt.Sprintf("%s*", periodicJobDir), fmt.Errorf("error listing gcs objects: %v", err))
-		}
+		go func(wg *sync.WaitGroup, ctx context.Context, periodicJobDir string, storageClient *storage.Client, opts options) {
+			defer wg.Done()
+			log.Infoln("starting report for ", periodicJobDir)
+			results, err := flakefinder.FindUnitTestFilesForPeriodicJob(ctx, storageClient, BucketName, []string{jobDir, periodicJobDir}, startOfReport, endOfReport)
+			if err != nil {
+				log.Printf("failed to load periodicJobDirs for %v: %v", fmt.Sprintf("%s*", periodicJobDir), fmt.Errorf("error listing gcs objects: %v", err))
+			}
 
-		type TestExecutions struct {
-			Name             string
-			TotalExecutions  int
-			FailedExecutions int
-		}
-
-		allTestExecutions := make(map[string]*TestExecutions)
-		testNames := []string{}
-		for _, result := range results {
-			for _, junit := range result.JUnit {
-				for _, test := range junit.Tests {
-					testName := flakefinder.NormalizeTestName(test.Name)
-					testExecutionRecord := &TestExecutions{
-						Name:             testName,
-						TotalExecutions:  0,
-						FailedExecutions: 0,
-					}
-					switch test.Status {
-					case junit2.StatusFailed, junit2.StatusError:
-						if _, exists := allTestExecutions[testName]; !exists {
-							allTestExecutions[testName] = testExecutionRecord
-							testNames = append(testNames, testName)
+			allTestExecutions := make(map[string]*testExecutions)
+			for _, result := range results {
+				for _, junit := range result.JUnit {
+					for _, test := range junit.Tests {
+						testName := flakefinder.NormalizeTestName(test.Name)
+						testExecutionRecord := &testExecutions{
+							Name:             testName,
+							TotalExecutions:  0,
+							FailedExecutions: 0,
 						}
-						testExecutionRecord = allTestExecutions[testName]
-						testExecutionRecord.FailedExecutions = testExecutionRecord.FailedExecutions + 1
-						testExecutionRecord.TotalExecutions = testExecutionRecord.TotalExecutions + 1
-					case junit2.StatusPassed:
-						if _, exists := allTestExecutions[testName]; !exists {
-							allTestExecutions[testName] = testExecutionRecord
-							testNames = append(testNames, testName)
+						switch test.Status {
+						case junit2.StatusFailed, junit2.StatusError:
+							if _, exists := allTestExecutions[testName]; !exists {
+								allTestExecutions[testName] = testExecutionRecord
+							}
+							testExecutionRecord = allTestExecutions[testName]
+							testExecutionRecord.FailedExecutions = testExecutionRecord.FailedExecutions + 1
+							testExecutionRecord.TotalExecutions = testExecutionRecord.TotalExecutions + 1
+						case junit2.StatusPassed:
+							if _, exists := allTestExecutions[testName]; !exists {
+								allTestExecutions[testName] = testExecutionRecord
+							}
+							testExecutionRecord = allTestExecutions[testName]
+							testExecutionRecord.TotalExecutions = testExecutionRecord.TotalExecutions + 1
+						default:
+							// NOOP
 						}
-						testExecutionRecord = allTestExecutions[testName]
-						testExecutionRecord.TotalExecutions = testExecutionRecord.TotalExecutions + 1
-					default:
-						// NOOP
 					}
 				}
 			}
-		}
-		sort.Strings(testNames)
 
-		file, err := os.CreateTemp("/tmp", fmt.Sprintf("per-test-execution-%s-%s-%s-*.csv", startOfReport.Format("2006-01"), endOfReport.Add(-1*24*time.Hour).Format("2006-01"), periodicJobDir))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		writer := csv.NewWriter(file)
-		writer.Write([]string{"test-name", "number-of-executions", "number-of-failures"})
-		for _, testName := range testNames {
-			testExecution, exists := allTestExecutions[testName]
-			if !exists {
-				log.Fatal("test %s doesn't exist", testName)
+			testNames := make([]string, 0, len(allTestExecutions))
+			for testName := range allTestExecutions {
+				testNames = append(testNames, testName)
 			}
-			err := writer.Write([]string{testExecution.Name, strconv.Itoa(testExecution.TotalExecutions), strconv.Itoa(testExecution.FailedExecutions)})
+			sort.Strings(testNames)
+
+			file, err := os.CreateTemp("/tmp", fmt.Sprintf("per-test-execution-%s-%s-%s-*.csv", startOfReport.Format("2006-01"), endOfReport.Add(-1*24*time.Hour).Format("2006-01"), periodicJobDir))
 			if err != nil {
 				log.Fatal(err)
 			}
-		}
-		log.Infoln("Execution data written to ", file.Name())
+			defer file.Close()
+			writer := csv.NewWriter(file)
+			writer.Write([]string{"test-name", "number-of-executions", "number-of-failures"})
+			for _, testName := range testNames {
+				testExecution, exists := allTestExecutions[testName]
+				if !exists {
+					log.Fatal("test %s doesn't exist", testName)
+				}
+				err := writer.Write([]string{testExecution.Name, strconv.Itoa(testExecution.TotalExecutions), strconv.Itoa(testExecution.FailedExecutions)})
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			writer.Flush()
+			log.Infoln("Execution data written to ", file.Name())
+		}(&wg, ctx, periodicJobDir, storageClient, opts)
 	}
+	wg.Wait()
 }
