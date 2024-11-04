51dba6412183ceace7b41aed2dcc7392716ac4eb
Brian Carey <bcarey@redhat.com>
2024-11-04 14:53:10 +0000

    prow: Manual bump of prow components (#3722)
    The prow autobump has not been picking up the new prow images - manually
    bump the images for now until this is resolved.
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>

--- hack/bump-prow.sh
+++ hack/bump-prow.sh
@@ -15,7 +15,7 @@ autobump() {
     relative_config_path="$1"
     # the below is necessary since running the autobumper inside a pod fails because of a failing git command
     (
-        podman run -v ${PROJECT_INFRA_ROOT}/:/config:z -v ${GITHUB_TOKEN_PATH}:/etc/github -it us-docker.pkg.dev/k8s-infra-prow/images/generic-autobumper:v20240802-66b115076 --config /config/${relative_config_path} --skip-pullrequest
+        podman run -v ${PROJECT_INFRA_ROOT}/:/config:z -v ${GITHUB_TOKEN_PATH}:/etc/github -it us-docker.pkg.dev/k8s-infra-prow/images/generic-autobumper:v20241029-f945d434d --config /config/${relative_config_path} --skip-pullrequest
     ) || true
 }
 
