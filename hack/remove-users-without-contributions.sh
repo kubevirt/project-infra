5e157a4697719313a79325a9a0b97696e3c7d09f
Daniel Hiller <dhiller@redhat.com>
2024-11-07 16:52:06 +0100

    kubevirt,governance: improve inactive users (#3693)
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/remove-users-without-contributions.sh
+++ hack/remove-users-without-contributions.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-set -exuo pipefail
+set -euo pipefail
 
-if [[ "$1" =~ -h ]]; then
+function usage() {
     cat << EOF
+
 usage: $0 <input-file>
 
     Removes all users without contributions (taken from the input file) from the orgs.yaml
@@ -13,14 +14,41 @@ usage: $0 <input-file>
 
     then from the top panel select "Inspect" > "Data" and select "Download CSV"
 EOF
+}
+
+if ! command -V yq; then
+    echo "yq is required!"
+    exit 1
+fi
+is_jq_wrapper="$(yq --help | grep -c 'jq wrapper for YAML documents')"
+
+if [ "$#" -lt 1 ]; then
+    echo "input-file is required"
+    usage
+    exit 1
+fi
+
+if [[ "$1" =~ -h ]]; then
+    usage
     exit 0
 fi
 
 if [ ! -f "$1" ]; then
-    echo "file $1 doesn't exist"
+    echo "input-file $1 doesn't exist"
+    usage
     exit 1
 fi
 input_csv_file="$1"
+
+set -x
+
+base_dir=$( cd $( dirname "${BASH_SOURCE[0]}") && pwd )
+
+orgs_yaml_path="${base_dir}/../github/ci/prow-deploy/kustom/base/configs/current/orgs/orgs.yaml"
+if [ ! -f "$orgs_yaml_path" ]; then
+    echo "File $orgs_yaml_path not found!"
+    exit 1
+fi
 
 # generate input file from csv where each user is in a separate line
 head -1 "$input_csv_file" \
@@ -56,11 +84,19 @@ aburdenthehand
 jberkus
 EOF
 
+function get_yaml_elements() {
+    if (( is_jq_wrapper == 1 )); then
+        yq -r ".$1[]" "$orgs_yaml_path"
+    else
+        yq read "$orgs_yaml_path" "$1"
+    fi
+}
+
 # iterate over org users (members and admins), grep over contributors list, remove every user not found
 (
     for username in $(
-        { yq read github/ci/prow-deploy/files/orgs.yaml orgs.kubevirt.members & \
-          yq read github/ci/prow-deploy/files/orgs.yaml orgs.kubevirt.admins ; \
+        { get_yaml_elements orgs.kubevirt.members & \
+          get_yaml_elements orgs.kubevirt.admins ; \
         } | sed 's/^- //' | sort -u); do
         echo $username "$(grep -i -c $username /tmp/users-with-contributions.txt)"
     done
@@ -70,7 +106,7 @@ EOF
 
 # remove all users without contributions
 for user in $(cat /tmp/users-without-contributions.txt); do
-    sed -i -E '/\s+- '"$user"'/d' github/ci/prow-deploy/kustom/base/configs/current/orgs/orgs.yaml
+    sed -i -E '/\s+- '"$user"'/d' "$orgs_yaml_path"
 done
 
 echo "Users without contributions:"
