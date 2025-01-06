763a0b065a44f9e52f9ff2720a334ebcd9f0f761
Daniel Hiller <dhiller@redhat.com>
2025-01-06 22:17:35 +0100

    cannier: cli command extract (mocked)
    Creates the first command `cannier extract` that defines the interface
    of the feature vector.
    
    Nearly all of the values returned are mocked.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ robots/cmd/cannier/cmd/extract.go
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright the KubeVirt Authors.
+ *
+ */
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"kubevirt.io/project-infra/robots/pkg/flake-heuristic/cannier"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	fileName            *string
+	outputFileName      *string
+	overwriteOutputFile *bool
+)
+
+func init() {
+	rootCmd.AddCommand(extractCmd)
+	fileName = extractCmd.Flags().StringP("filename", "f", "", "filename for the tests to analyze")
+	outputFileName = extractCmd.Flags().StringP("output-filename", "o", "", "filename to write the resulting feature set into, format is json")
+	overwriteOutputFile = extractCmd.Flags().BoolP("overwrite", "F", false, "whether to overwrite the output file if it exists")
+}
+
+// extractCmd represents the extract command
+var extractCmd = &cobra.Command{
+	Use:   "extract",
+	Short: "extracts a feature set from a single test",
+	Long:  `Extracts a feature set as described in the CANNIER paper from a single test.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return extractFeatures(*fileName, *outputFileName, *overwriteOutputFile)
+	},
+}
+
+func extractFeatures(fileName string, outputFileName string, overwriteOutputFile bool) error {
+	if fileName == "" {
+		return fmt.Errorf("filename is required")
+	}
+	if outputFileName == "" {
+		return fmt.Errorf("output-fileName is required")
+	}
+	_, err := os.Stat(outputFileName)
+	if !overwriteOutputFile && (err == nil || !os.IsNotExist(err)) {
+		return fmt.Errorf("output file %q must not exist", outputFileName)
+	}
+	features, err := cannier.ExtractFeatures(fileName)
+	if err != nil {
+		return fmt.Errorf("error extracting features: %w", err)
+	}
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return fmt.Errorf("error writing output file: %w", err)
+	}
+	defer outputFile.Close()
+	json.NewEncoder(outputFile).Encode(features)
+	return nil
+}
---
+++ robots/pkg/flake-heuristic/cannier/static-analysis.go
@@ -0,0 +1,173 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright the KubeVirt Authors.
+ *
+ */
+
+package cannier
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func getStaticAnalysisExtractors(filename string) ([]featureExtractor, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	code := string(content)
+
+	fs := token.NewFileSet()
+	node, err := parser.ParseFile(fs, filename, nil, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	return []featureExtractor{
+		func(featureSet *FeatureSet) error { featureSet.ASTDepth = calculateASTDepth(node); return nil },
+		func(featureSet *FeatureSet) error { featureSet.Assertions = countAssertions(code); return nil },
+		func(featureSet *FeatureSet) error {
+			featureSet.CyclomaticComplexity = calculateCyclomaticComplexity(node)
+			return nil
+		},
+		func(featureSet *FeatureSet) error {
+			featureSet.ExternalModules = countExternalModules(node)
+			return nil
+		},
+		func(featureSet *FeatureSet) error {
+			featureSet.TestLinesOfCode = countLinesOfCode(code)
+			return nil
+		},
+		func(featureSet *FeatureSet) error {
+			featureSet.HalsteadVolume = calculateHalsteadVolume(code)
+			return nil
+		},
+		calculateMaintainability,
+	}, nil
+}
+
+// calculateMaintainability calculates FeatureSet.Maintainability
+// TODO: stubbed
+func calculateMaintainability(featureSet *FeatureSet) error {
+	featureSet.Maintainability = 0.0
+	return nil
+}
+
+type maxDepthCapture struct {
+	maxDepth int
+}
+
+func (c *maxDepthCapture) updateIfGreater(depth int) {
+	if depth > c.maxDepth {
+		c.maxDepth = depth
+	}
+}
+
+type astVisitor struct {
+	depth int
+	*maxDepthCapture
+}
+
+func (v *astVisitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+	childDepth := v.depth + 1
+	v.maxDepthCapture.updateIfGreater(childDepth)
+	w := &astVisitor{depth: childDepth, maxDepthCapture: v.maxDepthCapture}
+	return w
+}
+
+// calculateASTDepth calculates the maximum AST depth.
+func calculateASTDepth(node ast.Node) int {
+	visitor := &astVisitor{depth: 0, maxDepthCapture: &maxDepthCapture{maxDepth: 0}}
+	ast.Walk(visitor, node)
+	return visitor.maxDepthCapture.maxDepth
+}
+
+// countAssertions counts the number of assertion statements.
+// TODO: add Ginkgo stuff
+func countAssertions(code string) int {
+	assertionRegex := regexp.MustCompile(`\b(require|assert)\.`)
+	return len(assertionRegex.FindAllString(code, -1))
+}
+
+// calculateCyclomaticComplexity calculates cyclomatic complexity.
+func calculateCyclomaticComplexity(node ast.Node) int {
+	complexity := 1
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch n.(type) {
+		case *ast.IfStmt, *ast.ForStmt, *ast.RangeStmt, *ast.SwitchStmt, *ast.CaseClause:
+			complexity++
+		}
+		return true
+	})
+	return complexity
+}
+
+// countExternalModules counts non-standard library imports.
+func countExternalModules(node ast.Node) int {
+	count := 0
+	ast.Inspect(node, func(n ast.Node) bool {
+		if imp, ok := n.(*ast.ImportSpec); ok {
+			if !strings.Contains(imp.Path.Value, "golang.org") && !strings.Contains(imp.Path.Value, "github.com") {
+				count++
+			}
+		}
+		return true
+	})
+	return count
+}
+
+// calculateHalsteadVolume calculates Halstead volume
+// TODO: stubbed with a simple formula
+func calculateHalsteadVolume(code string) float64 {
+	// Simple example using unique operators and operands
+	operators := regexp.MustCompile(`[+\-*/%=&|<>!]`).FindAllString(code, -1)
+	operands := regexp.MustCompile(`\b\w+\b`).FindAllString(code, -1)
+	n1 := len(operators)
+	n2 := len(operands)
+	N := n1 + n2
+	n := len(unique(operators)) + len(unique(operands))
+	if n == 0 {
+		return 0
+	}
+	return float64(N) * (float64(n) / float64(n1+n2))
+}
+
+// countLinesOfCode counts the total number of lines in the code.
+// TODO: stubbed with a simple formula
+func countLinesOfCode(code string) int {
+	return len(strings.Split(code, "\n"))
+}
+
+// unique returns a unique slice of strings.
+func unique(input []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range input {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
