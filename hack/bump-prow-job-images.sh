2781d7bb745f55f418212281dbbb15d32038ec32
Daniel Hiller <dhiller@redhat.com>
2025-01-17 14:58:46 +0100

    chore,image updates: fetch names from manifests (#3895)
    * chore,deployment updates: fetch image names from manifests
    
    Currently we rely on a fixed image list, which has to be modified
    whenever a new image is added.
    
    This change greps over the manifests and extracts all image names that
    match quay.io/kubevirtci - this list is then used as input for the
    update script.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    * chore,image update: update prow jobs from manifests
    
    We apply a similar change to the prow-jobs update script. Also we create
    a script for a pr description containing as detail the images that were
    updated in a list.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>
    
    ---------
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- hack/bump-prow-job-images.sh
+++ hack/bump-prow-job-images.sh
@@ -1,25 +1,46 @@
 #!/usr/bin/env bash
 
+#
+# This file is part of the KubeVirt project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright the KubeVirt Authors
+#
+#
+
 set -euo pipefail
 set -x
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROJECT_INFRA_ROOT=$(readlink --canonicalize ${BASEDIR}/..)
 
-# won't work as image names differ from directory names
-#IMAGES=$(
-#    ( cd "$PROJECT_INFRA_ROOT/images" && find . -mindepth 1 -maxdepth 1 -type d -print ) & ( cd "$PROJECT_INFRA_ROOT/robots/cmd" && find . -mindepth 1 -maxdepth 1 -type d -print ) ;
-#)
-# repos yet private
-#  kubekins-e2e  kubevirt-userguide
-# repos public but empty
-#  release-querier
-IMAGES=( autoowners bootstrap ci-usage-exporter flakefinder golang indexpagecreator kubevirt-infra-bootstrap prow-deploy pr-creator release-blocker )
-for image_dir in "${IMAGES[@]}"; do
-    image_name="quay.io/kubevirtci/${image_dir/#\.\//}"
-    if ! "$PROJECT_INFRA_ROOT/hack/update-jobs-with-latest-image.sh" "$image_name"; then
-        echo "Failed to update prow jobs using image $image_name"
-        exit 1
-    fi
-    echo "Updated prow jobs using image $image_name"
-done
+function main() {
+    for repo_name in $(kubevirtci_images_used_in_manifests); do
+        image_name="quay.io/kubevirtci/${repo_name}"
+        if ! "$PROJECT_INFRA_ROOT/hack/update-jobs-with-latest-image.sh" "$image_name"; then
+            echo "[FAIL] prow jobs update for image $image_name"
+        else
+            echo "[OK] prow jobs update for image $image_name"
+        fi
+    done
+}
+
+function kubevirtci_images_used_in_manifests() {
+    find "${PROJECT_INFRA_ROOT}/github/ci/prow-deploy" -name '*.yaml' -print0 | \
+        xargs -0 grep -hoE 'quay.io/kubevirtci/[^: @]+' | \
+        sort -u | \
+        cut -d'/' -f3
+}
+
+main "$@"
