3588d87f07d134127c049e809160f5a93886ef80
Daniel Hiller <dhiller@redhat.com>
2025-01-23 12:05:06 +0100

    cannier: random forest - generate dataset works
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- robots/cmd/cannier/cmd/model.go
+++ robots/cmd/cannier/cmd/model.go
@@ -20,11 +20,11 @@
 package cmd
 
 import (
-	"math/rand"
-	osexec "os/exec"
-
 	"github.com/malaschitz/randomForest"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"kubevirt.io/project-infra/robots/pkg/flake-heuristic/cannier"
+	"kubevirt.io/project-infra/robots/pkg/ginkgo"
 )
 
 var perTestExecutionCSVLinks = []string{
@@ -37,6 +37,12 @@ var perTestExecutionCSVLinks = []string{
 
 var defaultKubeVirtSourceCodeTestPath = "../kubevirt/tests"
 
+type ModelDataSources struct {
+	TestDescriptor
+	testFileName string
+	Features     *cannier.FeatureSet
+}
+
 // modelCmd represents the model command
 var modelCmd = &cobra.Command{
 	Use:   "model",
@@ -48,9 +54,10 @@ then uses the test names to extract the feature vectors, and then creates the
 dataset for the model. It finally trains the model and stores the model as a 
 serialized blob to given path.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// fetch tests from csv
-		descriptorsByName := make(map[string]TestDescriptor)
+		// fetch tests from csv, attach file names, then extract feature vector
+		dataSourcesByName := make(map[string]ModelDataSources)
 		for _, url := range perTestExecutionCSVLinks {
+			log.WithField("url", url).Infof("fetching test data")
 			records, err := ExtractSourceRecords(url)
 			if err != nil {
 				return err
@@ -60,33 +67,62 @@ serialized blob to given path.`,
 				return err
 			}
 			for _, descriptor := range descriptors {
-				descriptorsByName[descriptor.Name] = descriptor
-				// TODO: attach file
-				osexec.Command("git", "grep", "-E")
+				logTest := log.WithField("test", descriptor.Name)
+				logTest.Infof("Fetching data for test %q", descriptor.Name)
+				testFileName, err := ginkgo.FindTestFileByName(descriptor.Name, *testSourcePath)
+				if err != nil {
+					logTest.Warnf("could not find test file: %v", err)
+					continue
+				}
+				testDescriptor, err := ginkgo.NewTestDescriptorForName(descriptor.Name, testFileName)
+				if err != nil {
+					logTest.Warnf("could not create descriptor for file %q: %v", testFileName, err)
+					continue
+				}
+				logTest.Infof("extracting feature vector", descriptor.Name)
+				features, err := cannier.ExtractFeatures(testDescriptor)
+				if err != nil {
+					return err
+				}
+				newDataSource := ModelDataSources{
+					TestDescriptor: descriptor,
+					testFileName:   testFileName,
+					Features:       features,
+				}
+				dataSourcesByName[descriptor.Name] = newDataSource
 			}
 		}
 
-		// 3.) generate model from data
-		xData := [][]float64{}
-		yData := []int{}
-		for i := 0; i < 1000; i++ {
-			x := []float64{rand.Float64(), rand.Float64(), rand.Float64(), rand.Float64()}
-			y := int(x[0] + x[1] + x[2] + x[3])
+		// generate model from data
+		var xData [][]float64
+		var yData []int
+		log.Infof("generating model data")
+		for _, ds := range dataSourcesByName {
+			x := ds.Features.AsFloatVector()
+			y := ds.TestDescriptor.Label
 			xData = append(xData, x)
-			yData = append(yData, y)
+			yData = append(yData, int(y))
 		}
 		forest := randomforest.Forest{}
 		forest.Data = randomforest.ForestData{X: xData, Class: yData}
+		log.Infof("training model")
 		forest.Train(1000)
 
+		log.Infof("TODO: storing model")
 		// 4.) store model to disk via serialization
 		// https://stackoverflow.com/questions/51300011/how-to-write-a-struct-as-binary-data-to-a-file-in-golang
 		return nil
 	},
 }
 
+var (
+	testSourcePath *string
+	outputPath     *string
+)
+
 func init() {
 	generateCmd.AddCommand(modelCmd)
 
-	modelCmd.Flags().StringP("output-path", "o", "/tmp/kubevirt-cannier.bin", "Help message for toggle")
+	testSourcePath = modelCmd.Flags().StringP("test-source-path", "t", defaultKubeVirtSourceCodeTestPath, "Help message for toggle")
+	outputPath = modelCmd.Flags().StringP("output-path", "o", "/tmp/kubevirt-cannier.bin", "Help message for toggle")
 }
