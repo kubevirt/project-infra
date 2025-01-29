70537394cf2704534400d32589aca66591879eec
Brian Carey <bcarey@redhat.com>
2025-01-29 13:33:04 +0000

    prow,bump: include shared-images-controller in prow deployment bump  (#3923)
    * prow,bump: include shared-images-controller in prow deployment bump
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    * prow,bump: Add resulting bumps from running the updated script
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    ---------
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>

--- hack/update-deployments-with-latest-image.sh
+++ hack/update-deployments-with-latest-image.sh
@@ -11,20 +11,26 @@ image_tag=""
 if [ $# -gt 1 ]; then
     image_tag="$2"
 fi
-deployment_dir="$(readlink --canonicalize "$(cd "$(cd "$(dirname "$0")" && pwd)"'/../github/ci/prow-deploy/kustom/base/manifests/local' && pwd)")"
 
-IMAGE_NAME="$1"
-if [ -z "$image_tag" ]; then
-    latest_image_tag=$(skopeo list-tags "docker://$IMAGE_NAME" | jq -r '.Tags[] | select( match("^v?[0-9]+-[a-z0-9]{7,9}$") )' | tail -1)
-    if [ -z "$latest_image_tag" ]; then
-        echo "Couldn't find latest_image_tag"
-        exit 1
+declare -a deployment_dirs=('/../github/ci/prow-deploy/kustom/base/manifests/local' '/../github/ci/prow-deploy/kustom/overlays/kubevirt-prow-workloads/resources')
+
+for i in "${deployment_dirs[@]}";
+do
+    deployment_dir="$(readlink --canonicalize "$(cd "$(cd "$(dirname "$0")" && pwd)"$i && pwd)")"
+
+    IMAGE_NAME="$1"
+    if [ -z "$image_tag" ]; then
+        latest_image_tag=$(skopeo list-tags "docker://$IMAGE_NAME" | jq -r '.Tags[] | select( match("^v?[0-9]+-[a-z0-9]{7,9}$") )' | tail -1)
+        if [ -z "$latest_image_tag" ]; then
+            echo "Couldn't find latest_image_tag"
+            exit 1
+        fi
+        IMAGE_NAME_WITH_TAG="$IMAGE_NAME:$latest_image_tag"
+    else
+        IMAGE_NAME_WITH_TAG="$IMAGE_NAME:$image_tag"
     fi
-    IMAGE_NAME_WITH_TAG="$IMAGE_NAME:$latest_image_tag"
-else
-    IMAGE_NAME_WITH_TAG="$IMAGE_NAME:$image_tag"
-fi
 
-replace_regex='s#'"$IMAGE_NAME"'(@sha256\:|:v?[a-z0-9]+-).*$#'"$IMAGE_NAME_WITH_TAG"'#g'
+    replace_regex='s#'"$IMAGE_NAME"'(@sha256\:|:v?[a-z0-9]+-).*$#'"$IMAGE_NAME_WITH_TAG"'#g'
 
-find "$deployment_dir" -regextype egrep -regex '.*-deployment\.yaml' -exec sed -i -E "$replace_regex" {} +
+    find "$deployment_dir" -regextype egrep -regex '.*.yaml' -exec sed -i -E "$replace_regex" {} +
+done
