cb52a868af1d019cdbbb3a6dbe466b59fbd471c6
Daniel Hiller <dhiller@redhat.com>
2025-10-31 13:01:38 +0100

    feat(publish_image.sh): mount project-infra directory
    Instead of always checking out the main project-infra
    repository just use the locally mounted directory if available.
    
    This will enable to check the image build with the locally
    provided source, which in turn will enable a presubmit job for the
    image builds built from project-infra.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- images/publish_image.sh
+++ images/publish_image.sh
@@ -3,10 +3,18 @@
 
 main() {
     local build_only
-    while getopts "bh" opt; do
+    local no_cache
+    local project_infra_dir
+    project_infra_dir="$(readlink --canonicalize "$(pwd)/../")"
+    while getopts "bcp:h" opt; do
         case "$opt" in
             b)
                 build_only=true
+                ;;
+            c)
+                no_cache=true
+                ;;
+            p)  project_infra_dir="$OPTARG"
                 ;;
             h)
                 help
@@ -36,7 +44,7 @@ main() {
     (
         cd "$build_target"
 
-        build_image "$build_target" "$full_image_name"
+        build_image "$no_cache" "$build_target" "$full_image_name" "$project_infra_dir"
     )
     [[ $build_only ]] && return
     publish_image "$full_image_name"
@@ -52,6 +60,7 @@ help() {
     OPTIONS
         -h  Show this help message and exit.
         -b  Only build the image and exit. Do not publish the built image.
+        -p  Local project-infra directory
 EOF
 }
 
@@ -63,9 +72,18 @@ get_image_tag() {
 }
 
 build_image() {
-    local build_target="${1:?}"
-    local image_name="${2:?}"
-    podman build . -t "$image_name" -t "$build_target"
+    local no_cache="${1:-}"
+    local build_target="${2:?}"
+    local image_name="${3:?}"
+    local project_infra_dir="${4:-}"
+    build_args=""
+    if [ -d "$project_infra_dir" ]; then
+        build_args="-v $project_infra_dir:/project-infra"
+    fi
+    if [ "$no_cache" = "true" ]; then
+        build_args="$build_args --no-cache"
+    fi
+    podman build $build_args . -t "$image_name" -t "$build_target"
 }
 
 publish_image() {
