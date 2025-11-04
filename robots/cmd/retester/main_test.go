eecb4bed98ae74b8f4d9f7e63bda5c97c2e35d1d
Daniel Hiller <dhiller@redhat.com>
2025-11-04 15:36:47 +0100

    feat(retester): skip on non-required presubmits
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

---
+++ robots/cmd/retester/main_test.go
@@ -0,0 +1,140 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright The KubeVirt Authors.
+ * Copyright 2017 The Kubernetes Authors.
+ *
+ */
+
+package main
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestParseHTMLURL(t *testing.T) {
+	cases := []struct {
+		name string
+		url  string
+		org  string
+		repo string
+		num  int
+		fail bool
+	}{
+		{
+			name: "normal issue",
+			url:  "https://github.com/org/repo/issues/1234",
+			org:  "org",
+			repo: "repo",
+			num:  1234,
+		},
+		{
+			name: "normal pull",
+			url:  "https://github.com/pull-org/pull-repo/pull/5555",
+			org:  "pull-org",
+			repo: "pull-repo",
+			num:  5555,
+		},
+		{
+			name: "different host",
+			url:  "ftp://gitlab.whatever/org/repo/issues/6666",
+			org:  "org",
+			repo: "repo",
+			num:  6666,
+		},
+		{
+			name: "string issue",
+			url:  "https://github.com/org/repo/issues/future",
+			fail: true,
+		},
+		{
+			name: "weird issue",
+			url:  "https://gubernator.k8s.io/build/kubernetes-ci-logs/logs/ci-kubernetes-e2e-gci-gce/11947/",
+			fail: true,
+		},
+	}
+
+	for _, tc := range cases {
+		org, repo, num, err := parseHTMLURL(tc.url)
+		if err != nil && !tc.fail {
+			t.Errorf("%s: should not have produced error: %v", tc.name, err)
+		} else if err == nil && tc.fail {
+			t.Errorf("%s: failed to produce an error", tc.name)
+		} else {
+			if org != tc.org {
+				t.Errorf("%s: org %s != expected %s", tc.name, org, tc.org)
+			}
+			if repo != tc.repo {
+				t.Errorf("%s: repo %s != expected %s", tc.name, repo, tc.repo)
+			}
+			if num != tc.num {
+				t.Errorf("%s: num %d != expected %d", tc.name, num, tc.num)
+			}
+		}
+	}
+}
+
+func TestMakeQuery(t *testing.T) {
+	cases := []struct {
+		name       string
+		query      string
+		archived   bool
+		closed     bool
+		locked     bool
+		dur        time.Duration
+		expected   []string
+		unexpected []string
+	}{
+		{
+			name:       "basic query",
+			query:      "hello world",
+			expected:   []string{"hello world"},
+			unexpected: []string{"updated:"},
+		},
+		{
+			name:     "basic duration",
+			query:    "hello",
+			dur:      1 * time.Hour,
+			expected: []string{"hello", "updated:<"},
+		},
+		{
+			name:       "weird characters not escaped",
+			query:      "oh yeah!@#$&*()",
+			expected:   []string{"!", "@", "#", " "},
+			unexpected: []string{"%", "+"},
+		},
+		{
+			name:     "linebreaks are replaced by whitespaces",
+			query:    "label:foo\nlabel:bar",
+			expected: []string{"label:foo label:bar"},
+		},
+	}
+
+	for _, tc := range cases {
+		actual := makeQuery(tc.query, tc.dur)
+		for _, e := range tc.expected {
+			if !strings.Contains(actual, e) {
+				t.Errorf("%s: could not find %s in %s", tc.name, e, actual)
+			}
+		}
+		for _, u := range tc.unexpected {
+			if strings.Contains(actual, u) {
+				t.Errorf("%s: should not have found %s in %s", tc.name, u, actual)
+			}
+		}
+	}
+}
