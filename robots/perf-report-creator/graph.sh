defe6c451904c36ea38bd8d75160adef41ca0efb
Sreeja Varnam <134859702+Sreeja1725@users.noreply.github.com>
2026-01-20 19:40:16 +0530

    Plot weekly graphs for kwok perf tests (#4650)
    Signed-off-by: svarnam <svarnam@nvidia.com>

--- robots/perf-report-creator/graph.sh
+++ robots/perf-report-creator/graph.sh
@@ -22,7 +22,23 @@ perf-report-creator weekly-graph \
   --plotly-html=true \
   --since=2022-11-29 \
   --metrics-list=vmiCreationToRunningSecondsP50,vmiCreationToRunningSecondsP95,LIST-virtualmachineinstances-count,WATCH-virtualmachineinstances-count,LIST-pods-count,WATCH-pods-count,LIST-nodes-count,LIST-virtualmachineinstancemigrations-count,WATCH-virtualmachineinstancemigrations-count,LIST-endpoints-count,GET-virtualmachineinstances-count,GET-pods-count,GET-nodes-count,GET-virtualmachineinstancemigrations-count,GET-endpoints-count,CREATE-virtualmachineinstances-count,CREATE-pods-count,CREATE-nodes-count,CREATE-virtualmachineinstancemigrations-count,CREATE-endpoints-count,PATCH-virtualmachineinstances-count,PATCH-pods-count,PATCH-nodes-count,PATCH-virtualmachineinstancemigrations-count,PATCH-endpoints-count,UPDATE-virtualmachineinstances-count,UPDATE-pods-count,UPDATE-nodes-count,UPDATE-virtualmachineinstancemigrations-count,UPDATE-endpoints-count,avgVirtAPIMemoryUsageInMB,minVirtAPIMemoryUsageInMB,maxVirtAPIMemoryUsageInMB,avgVirtAPICPUUsage,avgVirtControllerMemoryUsageInMB,minVirtControllerMemoryUsageInMB,maxVirtControllerMemoryUsageInMB,avgVirtControllerCPUUsage,avgVirtHandlerMemoryUsageInMB,minVirtHandlerMemoryUsageInMB,maxVirtHandlerMemoryUsageInMB,avgVirtHandlerCPUUsage
-#
+
+## plot sig-performance-kwok ${KUBEVIRT_PROVIDER} weekly vmi
+perf-report-creator weekly-graph \
+  --resource=vmi \
+  --weekly-reports-dir=${OUTPUT_DIR}/weekly/periodic-kubevirt-e2e-${KUBEVIRT_PROVIDER}-sig-performance-kwok \
+  --plotly-html=true \
+  --since=2022-11-29 \
+  --metrics-list=vmiCreationToRunningSecondsP50,vmiCreationToRunningSecondsP95,LIST-virtualmachineinstances-count,LIST-pods-count,LIST-nodes-count,LIST-virtualmachineinstancemigrations-count,LIST-endpoints-count,GET-virtualmachineinstances-count,GET-pods-count,GET-nodes-count,GET-virtualmachineinstancemigrations-count,GET-endpoints-count,CREATE-virtualmachineinstances-count,CREATE-pods-count,CREATE-nodes-count,CREATE-virtualmachineinstancemigrations-count,CREATE-endpoints-count,PATCH-virtualmachineinstances-count,PATCH-pods-count,PATCH-nodes-count,PATCH-virtualmachineinstancemigrations-count,PATCH-endpoints-count,UPDATE-virtualmachineinstances-count,UPDATE-pods-count,UPDATE-nodes-count,UPDATE-virtualmachineinstancemigrations-count,UPDATE-endpoints-count,avgVirtAPIMemoryUsageInMB,minVirtAPIMemoryUsageInMB,maxVirtAPIMemoryUsageInMB,avgVirtAPICPUUsage,avgVirtControllerMemoryUsageInMB,minVirtControllerMemoryUsageInMB,maxVirtControllerMemoryUsageInMB,avgVirtControllerCPUUsage,avgVirtHandlerMemoryUsageInMB,minVirtHandlerMemoryUsageInMB,maxVirtHandlerMemoryUsageInMB,avgVirtHandlerCPUUsage
+
+## plot sig-performance-kwok ${KUBEVIRT_PROVIDER} weekly vm
+perf-report-creator weekly-graph \
+  --resource=vm \
+  --weekly-reports-dir=${OUTPUT_DIR}/weekly/periodic-kubevirt-e2e-${KUBEVIRT_PROVIDER}-sig-performance-kwok \
+  --plotly-html=true \
+  --since=2022-11-29 \
+  --metrics-list=vmiCreationToRunningSecondsP50,vmiCreationToRunningSecondsP95,LIST-virtualmachineinstances-count,LIST-pods-count,LIST-nodes-count,LIST-virtualmachineinstancemigrations-count,LIST-endpoints-count,GET-virtualmachineinstances-count,GET-pods-count,GET-nodes-count,GET-virtualmachineinstancemigrations-count,GET-endpoints-count,CREATE-virtualmachineinstances-count,CREATE-pods-count,CREATE-nodes-count,CREATE-virtualmachineinstancemigrations-count,CREATE-endpoints-count,PATCH-virtualmachineinstances-count,PATCH-pods-count,PATCH-nodes-count,PATCH-virtualmachineinstancemigrations-count,PATCH-endpoints-count,UPDATE-virtualmachineinstances-count,UPDATE-pods-count,UPDATE-nodes-count,UPDATE-virtualmachineinstancemigrations-count,UPDATE-endpoints-count,avgVirtAPIMemoryUsageInMB,minVirtAPIMemoryUsageInMB,maxVirtAPIMemoryUsageInMB,avgVirtAPICPUUsage,avgVirtControllerMemoryUsageInMB,minVirtControllerMemoryUsageInMB,maxVirtControllerMemoryUsageInMB,avgVirtControllerCPUUsage,avgVirtHandlerMemoryUsageInMB,minVirtHandlerMemoryUsageInMB,maxVirtHandlerMemoryUsageInMB,avgVirtHandlerCPUUsage
+  
 ## plot 100-density-test results
 perf-report-creator weekly-graph \
   --resource=vmi \
