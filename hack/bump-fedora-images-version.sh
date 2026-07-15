929ff2dde973241c9d61446fe8719b40d68fe35a
Brian Carey <brian.carey@protonmail.com>
2026-07-15 19:51:12 +0100

    kubevirtci: update bump fedora automation to also update Centos10 provision base image
    The CentOS 10 based provider did not exist when this automation was introduced. Include the CentOS 10 Containerfile in the bump automation to ensure that they stay in sync as currently they are not consistent.[1]
    
    Update the bump script to take multiple Containerfiles at once.
    
    [1] https://github.com/kubevirt/kubevirtci/blob/95e13d4a8cc165ef07ce1b2f87d2437e2c6b2267/cluster-provision/centos10/Dockerfile#L1
    
    Signed-off-by: Brian Carey <brian.carey@protonmail.com>

--- hack/bump-fedora-images-version.sh
+++ hack/bump-fedora-images-version.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 function usage {
     cat <<EOF
-usage: $0 <Containerfile/Dockerfile>
+usage: $0 <Containerfile/Dockerfile> [Containerfile/Dockerfile ...]
 
     Bump a fedora based container to the latest available fedora version
 
@@ -13,20 +13,20 @@ EOF
 }
 
 
-if [ ! "$#" -eq 1 ]; then
+if [ ! "$#" -ge 1 ]; then
     usage
     exit 1
 fi
 
-BUILD_FILE=$1
-
-if [ ! -f "$BUILD_FILE" ];  then
-    echo "Containerfile not found at $BUILD_FILE"
-    exit 1
-fi
+for BUILD_FILE in "$@"; do
+    if [ ! -f "$BUILD_FILE" ];  then
+        echo "Containerfile not found at $BUILD_FILE"
+        exit 1
+    fi
+done
 
 # Get latest version of Fedora available
 
 LATEST_FEDORA=$(curl -s -L https://fedoraproject.org/releases.json | jq -r '[.[].version|select(test("^[0-9]+$"))]|max')
 
-sed -i s"/fedora:[0-9][0-9]/fedora:$LATEST_FEDORA/"  $BUILD_FILE
+sed -i s"/fedora:[0-9][0-9]/fedora:$LATEST_FEDORA/" "$@"
