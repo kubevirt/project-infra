fbfbcda6c64561f386790a9d1f6ca906d0d199b5
Daniel Hiller <dhiller@redhat.com>
2026-07-16 17:19:49 +0200

    fix(imagebump): fix case of two tags with same date
    In case of images being bumped twice in one day, both images receive
    the same first part in their tag. Quay seems to return the list of tags
    in it's list-tag command response as sorted by tag alphabetically.
    However it is possible that a tag with the second part (an abbreviation
    of the commit hash) maybe sorted earlier, even though it has been pushed
    later in the day.
    
    This change adds a call to skopeo inspect fetching the Created date of
    the image as a fallback in case two images have the same first part.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- pkg/imagebump/skopeo.go
+++ pkg/imagebump/skopeo.go
@@ -25,38 +25,96 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
+	"sort"
+	"strings"
+	"time"
 )
 
 // kubevirtCITagPattern matches tags selected by hack/update-jobs-with-latest-image.sh
 var kubevirtCITagPattern = regexp.MustCompile(`^v?[0-9]+-[a-z0-9]{7,9}$`)
 var ErrNoMatchingTag = errors.New("no tag matching kubevirtci date-hash pattern")
 
-type skopeoListTags struct {
-	Tags []string `json:"Tags"`
-}
+type listTagsCommand func(imageRef string) ([]string, error)
 
-// LatestSkopeoTag returns the last tag in skopeo list-tags order that matches kubevirtCITagPattern
-func LatestSkopeoTag(imageRef string) (string, error) {
+var listTagsCmd = func(imageRef string) ([]string, error) {
 	cmd := exec.Command("skopeo", "list-tags", "docker://"+imageRef)
 	out, err := cmd.Output()
 	if err != nil {
 		if ee, ok := err.(*exec.ExitError); ok {
-			return "", fmt.Errorf("skopeo list-tags docker://%s: %w\n%s", imageRef, err, string(ee.Stderr))
+			return nil, fmt.Errorf("skopeo list-tags docker://%s: %w\n%s", imageRef, err, string(ee.Stderr))
 		}
-		return "", fmt.Errorf("skopeo list-tags docker://%s: %w", imageRef, err)
+		return nil, fmt.Errorf("skopeo list-tags docker://%s: %w", imageRef, err)
+	}
+	type skopeoListTags struct {
+		Tags []string `json:"Tags"`
 	}
 	var parsed skopeoListTags
 	if err := json.Unmarshal(out, &parsed); err != nil {
-		return "", fmt.Errorf("parse skopeo output: %w", err)
+		return nil, fmt.Errorf("parse skopeo output: %w", err)
 	}
-	var last string
-	for _, t := range parsed.Tags {
+	return parsed.Tags, nil
+}
+
+type inspectCommand func(fullImageRef string) (time.Time, error)
+
+var inspectCmd = func(fullImageRef string) (time.Time, error) {
+	cmd := exec.Command("skopeo", "inspect", "docker://"+fullImageRef)
+	out, err := cmd.Output()
+	if err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			return time.Time{}, fmt.Errorf("skopeo inspect docker://%s: %w\n%s", fullImageRef, err, string(ee.Stderr))
+		}
+		return time.Time{}, fmt.Errorf("skopeo inspect docker://%s: %w", fullImageRef, err)
+	}
+	type skopeoInspect struct {
+		Created time.Time `json:"Created"`
+	}
+	var parsed skopeoInspect
+	if err := json.Unmarshal(out, &parsed); err != nil {
+		return time.Time{}, fmt.Errorf("parse skopeo inspect output: %w", err)
+	}
+	return parsed.Created, nil
+}
+
+// LatestSkopeoTag returns the last tag in skopeo list-tags order that matches kubevirtCITagPattern
+// after sorting by the date part first - if that doesn't work, i.e. if the date part is the same for
+// two tags, then it resorts to calling skopeo inspect, and comparing the Created entry
+func LatestSkopeoTag(imageRef string) (string, error) {
+	tags, err := listTagsCmd(imageRef)
+	if err != nil {
+		return "", fmt.Errorf("listTagsCmd for %s: %w", imageRef, err)
+	}
+	var allMatching []string
+	for _, t := range tags {
 		if kubevirtCITagPattern.MatchString(t) {
-			last = t
+			allMatching = append(allMatching, t)
 		}
 	}
-	if last == "" {
+	if len(allMatching) == 0 {
 		return "", fmt.Errorf("%w for %s (pattern %s)", ErrNoMatchingTag, imageRef, kubevirtCITagPattern.String())
 	}
-	return last, nil
+
+	var sortErrs []error
+	sort.Slice(allMatching, func(i, j int) bool {
+		iDateTime, jDateTime := strings.Split(allMatching[i], "-")[0], strings.Split(allMatching[j], "-")[0]
+		if iDateTime != jDateTime {
+			return iDateTime < jDateTime
+		}
+		iCreated, err := inspectCmd(fmt.Sprintf("%s:%s", imageRef, allMatching[i]))
+		if err != nil {
+			sortErrs = append(sortErrs, err)
+			return false
+		}
+		jCreated, err := inspectCmd(fmt.Sprintf("%s:%s", imageRef, allMatching[j]))
+		if err != nil {
+			sortErrs = append(sortErrs, err)
+			return false
+		}
+		return iCreated.Before(jCreated)
+	})
+	if len(sortErrs) > 0 {
+		return "", fmt.Errorf("errors when sorting tags: %w", errors.Join(sortErrs...))
+	}
+
+	return allMatching[len(allMatching)-1], nil
 }
