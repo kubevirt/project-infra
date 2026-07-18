282a28c075cada62f190a989dda3abc7d1dbc636
Denis Ollier <dollierp@redhat.com>
2026-07-18 15:38:17 +0200

    chore(cleanup): remove unused Go code
    Signed-off-by: Denis Ollier <dollierp@redhat.com>

--- github/ci/services/common/k8s/pkg/client/client.go
+++ github/ci/services/common/k8s/pkg/client/client.go
@@ -5,8 +5,6 @@ import (
 	"os/user"
 	"path"
 
-	certmanagerclientset "github.com/jetstack/cert-manager/pkg/client/clientset/versioned"
-	apixv1client "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
 	"k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -19,24 +17,6 @@ func NewClientset() (*kubernetes.Clientset, error) {
 	}
 
 	return kubernetes.NewForConfig(config)
-}
-
-func NewApiextensionsV1Client() (*apixv1client.ApiextensionsV1Client, error) {
-	config, err := GetConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return apixv1client.NewForConfig(config)
-}
-
-func NewCertManagerClientset() (*certmanagerclientset.Clientset, error) {
-	config, err := GetConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return certmanagerclientset.NewForConfig(config)
 }
 
 func GetConfig() (*restclient.Config, error) {
